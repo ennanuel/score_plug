6c92c57ae7a3ff0208562be6fb2089b44e195615
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-03-02 14:16:12 +0100

    Moved files and updated tailwind config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,63 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          100: "#cdcdcd",
+          200: "#9b9b9b",
+          300: "#696969",
+          400: "#373737",
+          500: "#090909",
+          600: "#070707",
+          700: "#050505",
+          800: "#030303",
+          900: "#010101"
+        },
+        black: {
+          100: "#cdcdcd",
+          200: "#9b9b9b",
+          300: "#696969",
+          400: "#373737",
+          500: "#050505",
+          600: "#040404",
+          700: "#030303",
+          800: "#020202",
+          900: "#010101"
+        },
+        secondary: {
+          100: "#ffffff",
+          200: "#ffffff",
+          300: "#ffffff",
+          400: "#ffffff",
+          500: "#ffffff",
+          600: "#cccccc",
+          700: "#999999",
+          800: "#666666",
+          900: "#333333"
+        },
+        white: {
+          100: "#ffffff",
+          200: "#ffffff",
+          300: "#ffffff",
+          400: "#ffffff",
+          500: "#ffffff",
+          600: "#cccccc",
+          700: "#999999",
+          800: "#666666",
+          900: "#333333"
+        },
+        highlight: {
+          100: "#fee7cc",
+          200: "#fdce99",
+          300: "#fdb666",
+          400: "#fc9d33",
+          500: "#fb8500",
+          600: "#c96a00",
+          700: "#975000",
+          800: "#643500",
+          900: "#321b00"
+        }
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
@@ -17,4 +74,5 @@ const config: Config = {
   },
   plugins: [],
 };
+
 export default config;
