9c271c9f07fa420d1ec1527dce22f1d5615b7a9b
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-03-04 20:33:26 +0100

    Added styling and chaged tag name

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -2,9 +2,10 @@
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <footer className="flex flex-col gap-2 items-center justify-center border-t bg-primary-600 border-secondary-100/10 p-4">
+      <a className="text-lg text-secondary-200 font-semibold" href="https://github.com/ennanuel">Score Plug</a>
+      <p className="text-secondary-700 text-sm">Emmanuel Ezema, Copyright 2024.</p>
+    </footer>
   )
 }
 
