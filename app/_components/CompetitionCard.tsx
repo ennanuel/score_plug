14c6eaa6c98309c4d3a2c56d0dca31602f18a623
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-03-04 20:30:58 +0100

    Created competition card component

---
+++ app/_components/CompetitionCard.tsx
@@ -0,0 +1,29 @@
+import { Competition } from '@/types/competition.type';
+import Link from 'next/link';
+import Image from "next/image";
+import { MdKeyboardArrowRight, MdStar } from "react-icons/md";
+
+const CompetitionCard = ({ _id, name, emblem, area, matches, hasLiveMatch }: Competition) => {
+    return (
+        <Link href={`/competition/${_id}`} className={`flex items-center gap-4 justify-between bg-primary-800 hover:bg-primary-400/50 hover:border-transparent p-3 px-4 rounded-lg border ${hasLiveMatch ? 'border-highlight-400' : 'border-transparent'}`}>
+            <Image src={emblem} width={40} className="aspect-square object-contain" alt={name} />
+            <div className="flex-1 flex flex-col">
+                <div className="flex items-center gap-2">
+                    <h3 className="text-secondary-400 font-semibold">{name}</h3>
+                    <MdStar />
+                </div>
+                <span className="text-xs text-gray-500">{area.name}</span>
+            </div>
+            {
+                matches
+                    ? <p className={`text-xs font-bold ${hasLiveMatch ? 'text-highlight-400' : 'text-gray-400'}`}>
+                        {matches} {matches > 1 ? 'matches' : 'match'}
+                    </p>
+                    : null
+            }
+            <MdKeyboardArrowRight size={20} />
+        </Link>
+    )
+};
+
+export default CompetitionCard
