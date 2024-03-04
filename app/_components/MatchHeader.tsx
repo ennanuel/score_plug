d8b6fedd989952df59fd74c9cd9933eb02db0ee3
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-03-04 20:36:48 +0100

    Fixed 'use client' error

--- app/_components/MatchHeader.tsx
+++ app/_components/MatchHeader.tsx
@@ -1,20 +1,33 @@
-import { ChevronLeft, Notifications } from '@mui/icons-material'
+"use client";
+
+import { MdNotifications } from 'react-icons/md';
+import { FaAngleLeft } from 'react-icons/fa';
+import Image from "next/image";
+import AltHeader from './AltHeader';
+import barcelonaCrest from "../_assets/barcelona_crest.png";
+import liverpoolCrest from "../_assets/liverpool_crest.png";
+import { useParams } from 'next/navigation';
+import { getHeaderLinks } from '../_utils/link';
+import { MATCH_LINKS } from '../_assets/constants/match';
 
 const MatchHeader = () => {
+  const { id } = useParams<{ id: string }>();
+  const links = getHeaderLinks({ path: 'match', id, links: MATCH_LINKS });
+
   return (
     <>
       <div className="flex items-center justify-between gap-2 m-3 pr-3">
-        <ChevronLeft />
+        <FaAngleLeft />
         <div className="h-6 aspect-square border-2 border-secondary-800"></div>
         <div className="flex-1 flex flex-col">
           <h3 className="text-sm font-semibold">Premier League</h3>
           <p className='text-[.7em] text-secondary-700'>England</p>
         </div>
-        <Notifications />
+        <MdNotifications />
       </div>
-      <div className="bg-primary-600 p-4 grid grid-cols-5 m-3 mt-4 gap-2 rounded-md">
+      <div className="bg-gradient-to-r from-blue-900/50 to-red-900/50 p-4 grid grid-cols-5 m-3 mt-4 gap-2 rounded-md">
         <div className="col-span-2 flex flex-col gap-2 items-center justify-center">
-          <div className="h-20 aspect-square border-2 border-secondary-800 rounded-full" />
+          <Image width={90} src={barcelonaCrest} className="aspect-square object-contain" alt="Clug Crest" />
           <h3 className="font-bold text-sm text-secondary-600">FC Barcelona</h3>
         </div>
         <div className="col-span-1 flex flex-col items-center justify-center text-center">
@@ -22,23 +35,12 @@ const MatchHeader = () => {
           <p className="text-sm text-secondary-600">Starts in 2 hours</p>
         </div>
         <div className="col-span-2 flex flex-col gap-2 items-center justify-center">
-          <div className="h-20 aspect-square border-2 border-secondary-800 rounded-full" />
-          <h3 className="font-bold text-sm text-secondary-600">FC Barcelona</h3>
+          <Image width={90} src={liverpoolCrest} className="aspect-square object-contain" alt="Clug Crest" />
+          <h3 className="font-bold text-sm text-secondary-600">Liverpool FC</h3>
         </div>
       </div>
 
-      <ul className="flex items-center gap-2 mt-6 border-b border-secondary-900/60 px-4">
-        <li className="relative py-1 px-2">
-          <button className="font-bold text-highlight-400">Details</button>
-          <div className="absolute bottom-[-3px] left-0 h-[3px] w-full bg-highlight-500"></div>
-        </li>
-        <li className="relative py-1 px-2">
-          <button className="font-semibold text-secondary-700">Head to Head</button>
-        </li>
-        <li className="relative py-1 px-2">
-          <button className="font-semibold text-secondary-700">Prediction</button>
-        </li>
-      </ul>
+      <AltHeader links={links} />
     </>
   )
 }
