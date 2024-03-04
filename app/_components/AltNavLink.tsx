70f880eade1ba3a22493f6f9edc6878cc34ee6ac
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-03-04 20:30:22 +0100

    Created 'NavLink' component for 'AltHeader' component

---
+++ app/_components/AltNavLink.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+const NavLink = ({ href, title }: { href: string; title: string }) => {
+    const pathname = usePathname();
+    const active = pathname === href;
+
+    return (
+        <Link href={href} className={`relative flex items-center justify-center px-2 text-[.9rem] mb-[-1px] border ${active ? 'border-secondary-900/50 border-b-primary-900 bg-primary-900' : 'border-transparent hover:bg-white-100/5'}`}>
+            <span className={`${active ? 'text-secondary-500 font-semibold' : 'text-secondary-700'}`}>{title}</span>
+        </Link>
+    )
+};
+
+export default NavLink
