ed48469681b40b430faba8d16dd0eb83eba82dc8
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-03-05 18:36:05 +0100

    Refactored code

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
-import { useState } from 'react';
-import { StarOutline, ChevronRight, NotificationsActiveOutlined } from '@mui/icons-material';
+"use client";
 
-const Home = () => {
+import { useState } from 'react';
+import { MdArrowRight, MdFavorite, MdOutlineNotifications } from "react-icons/md";
+
+function Home() {
   const [live, setLive] = useState(false);
   const onLive = () => setLive(true);
   const offLive = () => setLive(false);
 
   return (
-    <div className="border border-secondary-900/50 bg-primary-500 p-3">
+    <main className="border border-secondary-900/50 bg-primary-500 p-3">
       <div className="flex items-center justify-between px-3">
         <div className="relative flex items-center justify-stretch h-[30px] gap-2">
           <button onClick={offLive} className={`relative flex justify-center items-center gap-2 h-full px-3 rounded-md font-semibold ${!live ? 'text-orange-300 bg-orange-400/20': 'bg-black-900/50 text-orange-700'}`}>
@@ -19,7 +21,7 @@ const Home = () => {
             <span>Live</span>
           </button>
         </div>
-        <StarOutline />
+        <MdFavorite />
       </div>
       <div className="bg-primary-800 py-3 px-4 rounded-lg mt-6">
         <h2 className="font-bold">Popular Leagues</h2>
@@ -30,7 +32,7 @@ const Home = () => {
               <span className="font-semibold text-secondary-500 text-sm">Premier League</span>
               <span className="text-[.7em] text-secondary-700">England</span>
             </div>
-            <ChevronRight />
+            <MdArrowRight />
           </div>
 
           <div className="flex items-center justify-between bg-secondary-900/60 hover:bg-secondary-900/80 rounded-md p-2">
@@ -49,27 +51,7 @@ const Home = () => {
               </div>
             </div>
             <div className="flex items-center justify-center pl-4 pr-2">
-              <NotificationsActiveOutlined />
-            </div>
-          </div>
-
-          <div className="flex items-center justify-between bg-secondary-900/60 hover:bg-secondary-900/80 rounded-md p-2">
-            <div className="h-[50px] w-[6px] rounded-md bg-highlight-500" />
-            <p className="px-4 py-3 text-highlight-400 font-semibold">18'</p>
-            <div className="flex flex-1 flex-col gap-2">
-              <div className="flex items-center gap-2">
-                <div className="w-4 aspect-square rounded-full border-2 border-secondary-800" />
-                <p className="flex-1 text-sm font-semibold">FC Barcelona</p>
-                <span className="font-bold text-sm">4</span>
-              </div>
-              <div className="flex items-center gap-2">
-                <div className="w-4 aspect-square rounded-full border-2 border-secondary-800" />
-                <p className="flex-1 text-sm font-semibold">FC Barcelona</p>
-                <span className="font-bold text-sm">4</span>
-              </div>
-            </div>
-            <div className="flex items-center justify-center pl-4 pr-2">
-              <NotificationsActiveOutlined />
+              <MdOutlineNotifications />
             </div>
           </div>
 
@@ -89,7 +71,27 @@ const Home = () => {
               </div>
             </div>
             <div className="flex items-center justify-center pl-4 pr-2">
-              <NotificationsActiveOutlined />
+              <MdOutlineNotifications />
+            </div>
+          </div>
+
+          <div className="flex items-center justify-between bg-secondary-900/60 hover:bg-secondary-900/80 rounded-md p-2">
+            <div className="h-[50px] w-[6px] rounded-md bg-highlight-500" />
+            <p className="px-4 py-3 text-highlight-400 font-semibold">18'</p>
+            <div className="flex flex-1 flex-col gap-2">
+              <div className="flex items-center gap-2">
+                <div className="w-4 aspect-square rounded-full border-2 border-secondary-800" />
+                <p className="flex-1 text-sm font-semibold">FC Barcelona</p>
+                <span className="font-bold text-sm">4</span>
+              </div>
+              <div className="flex items-center gap-2">
+                <div className="w-4 aspect-square rounded-full border-2 border-secondary-800" />
+                <p className="flex-1 text-sm font-semibold">FC Barcelona</p>
+                <span className="font-bold text-sm">4</span>
+              </div>
+            </div>
+            <div className="flex items-center justify-center pl-4 pr-2">
+              <MdOutlineNotifications />
             </div>
           </div>
         </div>
@@ -102,7 +104,7 @@ const Home = () => {
               <span className="font-semibold text-secondary-500 text-sm">La Liga</span>
               <span className="text-[.7em] text-secondary-700">Spain</span>
             </div>
-            <ChevronRight />
+            <MdArrowRight />
           </div>
 
           <div className="flex items-center justify-between bg-secondary-900/60 hover:bg-secondary-900/80 rounded-md p-2">
@@ -121,27 +123,7 @@ const Home = () => {
               </div>
             </div>
             <div className="flex items-center justify-center pl-4 pr-2">
-              <NotificationsActiveOutlined />
-            </div>
-          </div>
-
-          <div className="flex items-center justify-between bg-secondary-900/60 hover:bg-secondary-900/80 rounded-md p-2">
-            <div className="h-[50px] w-[6px] rounded-md bg-highlight-500" />
-            <p className="px-4 py-3 text-highlight-400 font-semibold">18'</p>
-            <div className="flex flex-1 flex-col gap-2">
-              <div className="flex items-center gap-2">
-                <div className="w-4 aspect-square rounded-full border-2 border-secondary-800" />
-                <p className="flex-1 text-sm font-semibold">FC Barcelona</p>
-                <span className="font-bold text-sm">4</span>
-              </div>
-              <div className="flex items-center gap-2">
-                <div className="w-4 aspect-square rounded-full border-2 border-secondary-800" />
-                <p className="flex-1 text-sm font-semibold">FC Barcelona</p>
-                <span className="font-bold text-sm">4</span>
-              </div>
-            </div>
-            <div className="flex items-center justify-center pl-4 pr-2">
-              <NotificationsActiveOutlined />
+              <MdOutlineNotifications />
             </div>
           </div>
 
@@ -161,14 +143,34 @@ const Home = () => {
               </div>
             </div>
             <div className="flex items-center justify-center pl-4 pr-2">
-              <NotificationsActiveOutlined />
+              <MdOutlineNotifications />
+            </div>
+          </div>
+
+          <div className="flex items-center justify-between bg-secondary-900/60 hover:bg-secondary-900/80 rounded-md p-2">
+            <div className="h-[50px] w-[6px] rounded-md bg-highlight-500" />
+            <p className="px-4 py-3 text-highlight-400 font-semibold">18'</p>
+            <div className="flex flex-1 flex-col gap-2">
+              <div className="flex items-center gap-2">
+                <div className="w-4 aspect-square rounded-full border-2 border-secondary-800" />
+                <p className="flex-1 text-sm font-semibold">FC Barcelona</p>
+                <span className="font-bold text-sm">4</span>
+              </div>
+              <div className="flex items-center gap-2">
+                <div className="w-4 aspect-square rounded-full border-2 border-secondary-800" />
+                <p className="flex-1 text-sm font-semibold">FC Barcelona</p>
+                <span className="font-bold text-sm">4</span>
+              </div>
+            </div>
+            <div className="flex items-center justify-center pl-4 pr-2">
+              <MdOutlineNotifications />
             </div>
           </div>
         </div>
 
       </div>
 
-    </div>
+    </main>
   )
 }
 
