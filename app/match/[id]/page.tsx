da91cb990fddc5bd7b6e6e5553922206ec3be805
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-03-10 14:05:22 +0100

    Removed unnecessary component

--- app/match/[id]/page.tsx
+++ app/match/[id]/page.tsx
@@ -26,22 +26,6 @@ const MatchInfo = () => {
           <span className="text-sm font-semibold">Italy</span>
         </p>
       </div>
-
-      <p className='text-center font-semibold text-secondary-500 mt-6 pt-2'>Other Matches You Might Like</p>
-      <div className="bg-primary-600 p-4 grid grid-cols-5 m-3 mt-4 gap-2 rounded-md">
-        <div className="col-span-2 flex flex-col gap-2 items-center justify-center">
-          <div className="h-[50px] aspect-square border-2 border-secondary-800 rounded-full" />
-          <h3 className="font-bold text-xs text-secondary-600">FC Barcelona</h3>
-        </div>
-        <div className="col-span-1 flex flex-col items-center justify-center text-center">
-          <p className="text-xl font-bold">22:00</p>
-          <p className="text-xs text-secondary-600">Starts in 2 hours</p>
-        </div>
-        <div className="col-span-2 flex flex-col gap-2 items-center justify-center">
-          <div className="h-[50px] aspect-square border-2 border-secondary-800 rounded-full" />
-          <h3 className="font-bold text-xs text-secondary-600">FC Barcelona</h3>
-        </div>
-      </div>
     </div>
   )
 }
