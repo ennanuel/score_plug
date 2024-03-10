0518cdc5a0aa5d0887fce175ca74f382743e9aca
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-03-10 14:17:55 +0100

    Removed 'path' as prop from 'DateAndStatusFilter' component

--- app/competition/[id]/matches/page.tsx
+++ app/competition/[id]/matches/page.tsx
@@ -1,17 +1,12 @@
-"use client"
-
 import { MATCHES } from '@/app/_assets/constants/match';
 import { DateAndStatusFilter, MatchCard } from '@/app/_components';
 import { Match } from '@/types/match.type';
-import { usePathname } from 'next/navigation';
 
 const CompetitionMatches = () => {
-    const pathname = usePathname();
-
     return (
         <div className="p-2">
             <div className="flex flex-col gap-3 mt-4 p-2 border border-secondary-900/50">
-                <DateAndStatusFilter path={pathname} />
+                <DateAndStatusFilter />
                 <ul className="flex gap-1 flex-col">
                     {
                         MATCHES.map((match, index) => (
