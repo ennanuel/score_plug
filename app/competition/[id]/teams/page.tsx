7867979c5ac44bcff72787c85a845399500affba
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-03-10 14:01:53 +0100

    Added placeholder components for competition teams page

--- app/competition/[id]/teams/page.tsx
+++ app/competition/[id]/teams/page.tsx
@@ -1,25 +1,15 @@
-
+import { TEAMS } from "@/app/_assets/constants/team";
+import { TeamCard } from "@/app/_components";
 
 const CompetitionTeams = () => {
   return (
-    <div className="grid grid-cols-5 gap-4 p-4">
-      <div className="flex flex-col items-center p-3 gap-3 bg-primary-600 rounded-md hover:bg-primary-500 border border-highlight-400">
-        <div className="w-full aspect-square rounded-full bg-white-900/50 relative">
-          <div className="absolute top-0 right-0 w-3 aspect-square rounded-full bg-highlight-500"></div>
-        </div>
-        <p className="font-semibold text-secondary-600 truncate text-sm">FC Barcelona</p>
-      </div>
-      <div className="flex flex-col items-center p-3 gap-3 bg-primary-600 rounded-md hover:bg-primary-500">
-        <div className="w-full aspect-square rounded-full bg-white-900/50 relative">
-        </div>
-        <p className="font-semibold text-secondary-600 truncate text-sm">FC Barcelona</p>
-      </div>
-      <div className="flex flex-col items-center p-3 gap-3 bg-primary-600 rounded-md hover:bg-primary-500">
-        <div className="w-full aspect-square rounded-full bg-white-900/50 relative">
-        </div>
-        <p className="font-semibold text-secondary-600 truncate text-sm">FC Barcelona</p>
-      </div>
-    </div>
+    <ul className="grid grid-cols-5 gap-4 p-4">
+      {
+        TEAMS.map((team, index) => (
+          <li key={index}><TeamCard {...team} /></li>
+        ))
+      }
+    </ul>
   )
 }
 
