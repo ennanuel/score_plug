d381c26e20a92712733e677774cf0ab87081c6cc
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-04-09 05:13:23 +0100

    changed the name of some props

--- app/_components/CompetitionCard.tsx
+++ app/_components/CompetitionCard.tsx
@@ -3,9 +3,9 @@ import Link from 'next/link';
 import Image from "next/image";
 import { MdKeyboardArrowRight, MdStar } from "react-icons/md";
 
-const CompetitionCard = ({ _id, name, emblem, area, noOfMatches, hasLiveMatch }: Competition) => {
+const CompetitionCard = ({ _id, name, emblem, area, recentMatches }: Competition) => {
     return (
-        <Link href={`/competition/${_id}`} className={`flex items-center gap-4 justify-between bg-primary-800 hover:bg-primary-400/50 hover:border-transparent p-3 px-4 rounded-lg border ${hasLiveMatch ? 'border-highlight-400' : 'border-transparent'}`}>
+        <Link href={`/competition/${_id}`} className={`flex items-center gap-4 justify-between bg-primary-800 hover:bg-primary-400/50 hover:border-transparent p-3 px-4 rounded-lg border ${recentMatches.hasLiveMatch ? 'border-highlight-400' : 'border-transparent'}`}>
             <Image src={emblem} width={40} className="aspect-square object-contain" alt={name} />
             <div className="flex-1 flex flex-col">
                 <div className="flex items-center gap-2">
@@ -15,9 +15,9 @@ const CompetitionCard = ({ _id, name, emblem, area, noOfMatches, hasLiveMatch }:
                 <span className="text-xs text-gray-500">{area.name}</span>
             </div>
             {
-                noOfMatches
-                    ? <p className={`text-xs font-bold ${hasLiveMatch ? 'text-highlight-400' : 'text-gray-400'}`}>
-                        {noOfMatches} {noOfMatches > 1 ? 'matches' : 'match'}
+                Boolean(recentMatches.matches)
+                    ? <p className={`text-xs font-bold ${recentMatches.hasLiveMatch ? 'text-highlight-400' : 'text-gray-400'}`}>
+                        {recentMatches.matches} {recentMatches.matches > 1 ? 'matches' : 'match'}
                     </p>
                     : null
             }
