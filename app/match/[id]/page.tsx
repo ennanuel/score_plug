bc08b283874ca4dfc273ccc3526dc8b06dcd0425
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-04-09 06:20:41 +0100

    Added and removed fields from graphql fetch query

--- app/match/[id]/page.tsx
+++ app/match/[id]/page.tsx
@@ -4,44 +4,13 @@ import React from 'react';
 import { Match } from '@/types/match.type';
 import { gql, useQuery } from '@apollo/client';
 import { useParams } from 'next/navigation';
+import { ErrorMessage, LoadingMessage } from '@/app/_components';
 
 
 const query = gql`
   query GetMatchByID($id: ID!) {
     match(id: $id) {
-      _id
-      minute
-
-      competition {
-        _id
-        name
-        area {
-          name
-          flag
-        }
-      }
-
-      homeTeam {
-        _id
-        name
-        crest
-      }
-      awayTeam {
-        _id
-        name
-        crest
-      }
-
-      score {
-        firstHalf {
-          home
-          away
-        }
-        fullTime {
-          home
-          away
-        }
-      }
+      venue
 
       referees {
         name
@@ -59,26 +28,36 @@ const MatchInfo = () => {
     variables: { id }
   });
 
-  console.log(data);
+  if (loading) return <LoadingMessage />;
+  else if (error) return <ErrorMessage />;
+  else if (!data) return null;
 
   return (
     <div className='mt-2 p-4'>
-      <p className='text-center font-semibold text-secondary-500'>Referee</p>
-      <div className="p-4 py-3 bg-gradient-to-br from-primary-500/40 to-primary-600 rounded-md flex flex-col gap-4 mt-2">
-        <p className="flex items-center justify-between">
-          <span className='text-sm text-secondary-600'>Name</span>
-          <span className="text-sm font-semibold">Jorginho Rodriguez</span>
-        </p>
-        <p className="flex items-center justify-between">
-          <span className='text-sm text-secondary-600'>Nationality</span>
-          <span>County Flag</span>
-          <span className="text-sm font-semibold">Italy</span>
-        </p>
-      </div>
+      <ul className="flex flex-col gap-2">
+        {
+          data.match.referees.map((referee, index) => (
+            <li key={index}>
+              <p className='text-center font-semibold text-secondary-500'>Referee</p>
+              <div className="p-4 py-3 bg-gradient-to-br from-primary-500/40 to-primary-600 rounded-md flex flex-col gap-4 mt-2">
+                <p className="flex items-center justify-between">
+                  <span className='text-sm text-secondary-600'>Name</span>
+                  <span className="text-sm font-semibold">{referee.name}</span>
+                </p>
+                <p className="flex items-center justify-between">
+                  <span className='text-sm text-secondary-600'>Nationality</span>
+                  <span>County Flag</span>
+                  <span className="text-sm font-semibold">{referee.natitionaliity}</span>
+                </p>
+              </div>
+            </li>
+          ))
+        }
+      </ul>
 
       <div className="flex-col gap-2">
         <h3>Venue</h3>
-        <p>Metro Politano, Italy</p>
+        <p>{data.match.venue}</p>
       </div>
     </div>
   )
