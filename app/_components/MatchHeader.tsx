343b2af17a5fe3535ce42281a4083f9e4d3f830e
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-04-09 06:14:51 +0100

    Utilized GraphQL query to fetch data

--- app/_components/MatchHeader.tsx
+++ app/_components/MatchHeader.tsx
@@ -10,9 +10,77 @@ import { useParams } from 'next/navigation';
 import { getHeaderLinks } from '../_utils/link';
 import { MATCH_LINKS } from '../_assets/constants/match';
 
+import { gql, useQuery } from '@apollo/client';
+import { Match } from "@/types/match.type";
+import LoadingMessage from './LoadingMessage';
+import ErrorMessage from './ErrorMessage';
+import { useMemo } from 'react';
+import { getTimeFormat } from '../_utils/dateTime';
+
+const query = gql`
+  query GetMatchByID($id: ID!) {
+    match(id: $id) {
+      _id
+      minute
+      timeRemaining
+
+      competition {
+        _id
+        name
+        area {
+          name
+          flag
+        }
+      }
+
+      homeTeam {
+        _id
+        name
+        crest
+        clubColors
+      }
+      awayTeam {
+        _id
+        name
+        crest
+        clubColors
+      }
+
+      score {
+        firstHalf {
+          home
+          away
+        }
+        fullTime {
+          home
+          away
+        }
+      }
+
+      referees {
+        name
+        type
+        nationality
+      }
+
+    }
+  }
+`
+
 const MatchHeader = () => {
   const { id } = useParams<{ id: string }>();
   const links = getHeaderLinks({ path: 'match', id, links: MATCH_LINKS });
+
+  const { loading, error, data } = useQuery<{ match: Match }>(query, {
+    variables: { id }
+  });
+  const time = useMemo(() => getTimeFormat(data?.match.utcDate || ''), [data]);
+
+  if (loading) return <LoadingMessage />;
+  else if (error) return <ErrorMessage />;
+  else if (!data) return null;
+
+  console.log(data.match.homeTeam.clubColors, data.match.awayTeam.clubColors)
 
   return (
     <>
@@ -20,23 +88,25 @@ const MatchHeader = () => {
         <FaAngleLeft />
         <div className="h-6 aspect-square border-2 border-secondary-800"></div>
         <div className="flex-1 flex flex-col">
-          <h3 className="text-sm font-semibold">Premier League</h3>
-          <p className='text-[.7em] text-secondary-700'>England</p>
+          <h3 className="text-sm font-semibold">{data.match.competition.name}</h3>
+          <p className='text-[.7em] text-secondary-700'>{data.match.competition.area.name}</p>
         </div>
-        <MdNotifications />
+        <MdNotifications size={20} />
       </div>
       <div className="bg-gradient-to-r from-blue-900/50 to-red-900/50 p-4 grid grid-cols-5 m-3 mt-4 gap-2 rounded-md">
         <div className="col-span-2 flex flex-col gap-2 items-center justify-center">
-          <Image width={90} src={barcelonaCrest} className="aspect-square object-contain" alt="Clug Crest" />
-          <h3 className="font-bold text-sm text-secondary-600">FC Barcelona</h3>
+          <Image width={90} src={data.match.homeTeam.crest} className="aspect-square object-contain" alt="Clug Crest" />
+          <h3 className="font-bold text-sm text-secondary-600">{data.match.homeTeam.name}</h3>
         </div>
         <div className="col-span-1 flex flex-col items-center justify-center text-center">
-          <p className="text-3xl font-bold">22:00</p>
-          <p className="text-sm text-secondary-600">Starts in 2 hours</p>
+          <p className="text-3xl font-bold">{time}</p>
+          <p className="text-sm text-secondary-600">
+            Starts in {Object.values(data.match.timeRemaining).find((value) => (value as number) >= 1)} {Object.entries(data.match.timeRemaining).find(([key, value]) => (value as number) >= 1)?.slice(0, 1)}
+          </p>
         </div>
         <div className="col-span-2 flex flex-col gap-2 items-center justify-center">
-          <Image width={90} src={liverpoolCrest} className="aspect-square object-contain" alt="Clug Crest" />
-          <h3 className="font-bold text-sm text-secondary-600">Liverpool FC</h3>
+          <Image width={90} src={data.match.homeTeam.crest} className="aspect-square object-contain" alt="Clug Crest" />
+          <h3 className="font-bold text-sm text-secondary-600">{data.match.awayTeam.name}</h3>
         </div>
       </div>
 
