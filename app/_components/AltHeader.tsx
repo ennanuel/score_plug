14d25a9fb987a534b7572e887f86bfc5028d1721
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-04-10 22:36:42 +0100

    modified styling

--- app/_components/AltHeader.tsx
+++ app/_components/AltHeader.tsx
@@ -3,7 +3,7 @@ import AltNavLink from "./AltNavLink";
 const AltHeader = ({ links }: { links: { title: string; href: string; }[] }) => {
 
     return (
-        <ul className="flex px-2 border-b border-secondary-900/50 sticky top-[55px] h-[40px] bg-primary-900">
+        <ul className="flex px-2 border-b border-secondary-900/50 sticky top-[45px] h-[40px] bg-primary-900">
             {links.map((link, index) => <AltNavLink key={index} {...link} />)}
         </ul>
     )
