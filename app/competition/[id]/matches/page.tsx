c3544723b458cdd17204d95b11fc96ec470ec150
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-04-11 22:51:04 +0100

    utilized graphql query and imported MatchesContainer

--- app/competition/[id]/matches/page.tsx
+++ app/competition/[id]/matches/page.tsx
@@ -1,21 +1,51 @@
-import { MATCHES } from '@/app/_assets/constants/match';
-import { DateAndStatusFilter, MatchCard } from '@/app/_components';
-import { Match } from '@/types/match.type';
+import { DateAndStatusFilter, LoadingMessage, ErrorMessage, MatchesContainer } from '@/app/_components';
+import { gql, useQuery } from '@apollo/client';
+import { useParams } from 'next/navigation';
 import { Suspense } from 'react';
 
+const QUERY = gql`
+    query CompetitionMatches($id: ID!) {
+        competition(id: $id) {
+            matches {
+                _id
+                utcDate
+                minute
+                status
+                homeTeam {
+                    _id
+                    name
+                    crest
+                }
+                awayTeam {
+                    _id
+                    name
+                    crest
+                }
+                score {
+                    fullTime {
+                        home
+                        away
+                    }
+                }
+            }
+        }
+    }
+`
+
 const CompetitionMatches = () => {
+    const { id } = useParams();
+    const { loading, error, data } = useQuery(QUERY, { variables: { id } });
+
+    if (loading) return <LoadingMessage />;
+    else if (error) return <ErrorMessage />;
+    else if (!data) return <div>Nothing was found</div>;
+
     return (
         <Suspense>
             <div className="p-2">
                 <div className="flex flex-col gap-3 mt-4 p-2 border border-secondary-900/50">
                     <DateAndStatusFilter />
-                    <ul className="flex gap-1 flex-col">
-                        {
-                            MATCHES.map((match, index) => (
-                                <li key={index}><MatchCard {...(match as Match)} /></li>
-                            ))
-                        }
-                    </ul>
+                    <MatchesContainer matches={data.competition.matches} />
                 </div>
             </div>
         </Suspense>
