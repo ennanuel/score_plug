79f6cb26055ac82f84f83a9228acb9dcf3a8e629
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-04-13 17:16:29 +0100

    styled border again

--- app/_components/CompetitionCard.tsx
+++ app/_components/CompetitionCard.tsx
@@ -5,7 +5,7 @@ import { MdKeyboardArrowRight, MdStar } from "react-icons/md";
 
 const CompetitionCard = ({ _id, name, emblem, area, recentMatches }: Competition) => {
     return (
-        <Link href={`/competition/${_id}`} className={`flex items-center gap-4 justify-between bg-primary-800 hover:bg-primary-400/50 hover:border-transparent p-2 rounded-sm border ${recentMatches.hasLiveMatch ? 'border-highlight-400/20' : 'border-transparent'}`}>
+        <Link href={`/competition/${_id}`} className={`flex items-center gap-4 justify-between bg-transparent hover:bg-primary-400/10 p-2 rounded-sm border ${recentMatches.hasLiveMatch ? 'border-highlight-400/20' : 'border-primary-400/10'}`}>
             <Image src={emblem || String(process.env.NEXT_IMAGE_URI)} width={30} height={30} className="aspect-square object-contain" alt={name} />
             <div className="flex-1 flex flex-col">
                 <div className="flex items-center gap-2">
