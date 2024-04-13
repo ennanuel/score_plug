cd8a1e413d6f1eb08ed4c2623479f0dd1449a48b
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-04-13 09:45:32 +0100

    styled percentage

--- app/match/[id]/prediction/page.tsx
+++ app/match/[id]/prediction/page.tsx
@@ -177,14 +177,14 @@ const MatchPrediction = () => {
                   <li className="flex flex-col gap-1">
                     <div className="flex items-center gap-2">
                       <p className="w-14 text-xs text-secondary-600">Ov. {goalsOutcome[key as keyof typeof goalsOutcome]}</p>
-                      <div style={{ width: `calc(${value.over}% - 20px)` }} className="w-full min-w-[60px] h-[25px] flex items-center justify-end border border-highlight-400 bg-highlight-400/10 px-2">
-                        <p className="text-highlight-400 font-semibold text-xs">{value.over?.toFixed(2)}%</p>
+                      <div style={{ width: `calc(${value.over}% - 40px)` }} className="w-full min-w-[60px] h-[25px] flex items-center justify-end border border-highlight-400 bg-highlight-400/10 px-2">
+                        <p className="text-highlight-400 font-semibold text-xs">{value.over?.toFixed(2)} <span className="font-normal">%</span></p>
                       </div>
                     </div>
                     <div className="flex items-center gap-2">
                       <p className="w-14 text-xs text-secondary-600">Un. {goalsOutcome[key as keyof typeof goalsOutcome]}</p>
-                      <div style={{ width: `calc(${value.under}% - 20px)` }} className="w-full min-w-[60px]  h-[25px] flex items-center justify-end border border-highlight-600 bg-highlight-600/10 px-2">
-                        <p className="text-highlight-600 font-semibold text-xs">{value.under?.toFixed(2)}%</p>
+                      <div style={{ width: `calc(${value.under}% - 40px)` }} className="w-full min-w-[60px]  h-[25px] flex items-center justify-end border border-highlight-600 bg-highlight-600/10 px-2">
+                        <p className="text-highlight-600 font-semibold text-xs">{value.under?.toFixed(2)} <span className="font-normal">%</span></p>
                       </div>
                     </div>
                   </li>
