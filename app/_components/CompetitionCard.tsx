c54dff1615a141052f7765dd0ff3641635e0f265
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-04-13 14:48:59 +0100

    styled card differently

--- app/_components/CompetitionCard.tsx
+++ app/_components/CompetitionCard.tsx
@@ -5,14 +5,18 @@ import { MdKeyboardArrowRight, MdStar } from "react-icons/md";
 
 const CompetitionCard = ({ _id, name, emblem, area, recentMatches }: Competition) => {
     return (
-        <Link href={`/competition/${_id}`} className={`flex items-center gap-4 justify-between bg-primary-800 hover:bg-primary-400/50 hover:border-transparent p-3 px-4 rounded-lg border ${recentMatches.hasLiveMatch ? 'border-highlight-400' : 'border-transparent'}`}>
-            <Image src={emblem || String(process.env.NEXT_IMAGE_URI)} width={40} className="aspect-square object-contain" alt={name} />
+        <Link href={`/competition/${_id}`} className={`flex items-center gap-4 justify-between bg-primary-800 hover:bg-primary-400/50 hover:border-transparent p-2 rounded-sm border ${recentMatches.hasLiveMatch ? 'border-highlight-400' : 'border-transparent'}`}>
+            <div className="w-[50px] h-[50px] aspect-square rounded-full bg-gradient-to-br from-white-100/5 to-white-100/20">
+                <Image src={emblem || String(process.env.NEXT_IMAGE_URI)} width={40} height={40} className="aspect-square object-contain" alt={name} />
+            </div>
             <div className="flex-1 flex flex-col">
                 <div className="flex items-center gap-2">
-                    <h3 className="text-secondary-400 font-semibold">{name}</h3>
-                    <MdStar />
+                    <h3 className="text-secondary-400 font-semibold text-sm">{name}</h3>
                 </div>
-                <span className="text-xs text-gray-500">{area.name}</span>
+                <div className="flex items-center gap-2">
+                    <Image src={area.flag || String(process.env.NEXT_IMAGE_URI)} width={15} height={15} className="object-contain" alt="Competition area flag" />
+                    <span className="text-xs text-gray-500">{area.name}</span>
+                </div>
             </div>
             {
                 Boolean(recentMatches.matches)
