65802c778fbf8f34046fb12c93b7ad4b322c2387
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-04-14 18:51:51 +0100

    ...

--- app/_components/standings/index.tsx
+++ app/_components/standings/index.tsx
@@ -20,7 +20,7 @@ function Standings({ competition, teams = [] }: { competition?: Competition, tea
             {
                 competition.standings.map((standing) => (
                     <div>
-                        {competition.type === 'CUP' && <h3 className="mt-4 p-2 border-t border-white-100/10 bg-white-100/5 font-semibold text-sm">{standing.group}</h3>}
+                        {competition.type === 'CUP' && <h3 className="mt-4 p-2 border-t border-white-100/5 bg-white-100/5 font-semibold text-sm">{standing.group}</h3>}
                     <table className="w-full">
                         <thead className="text-xs text-secondary-700 h-8 border-y border-secondary-900/50">
                         <th className="w-[50px] font-semibold text-center">
