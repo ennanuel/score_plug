c8a0f86b5cd3cb81bcbe5dd178afca93eed73927
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-04-14 18:29:28 +0100

    ...

--- app/match/[id]/prediction/page.tsx
+++ app/match/[id]/prediction/page.tsx
@@ -170,16 +170,16 @@ const MatchPrediction = () => {
               Object.entries(data.match.predictions[timePeriod].goals)
                 .filter(([key, { over, under }]) => over || under)
                 .map(([key, value]) => (
-                  <li className="flex flex-col gap-1 px-4 p-2 border-b last:border-b-0 border-white-100/10">
+                  <li className="flex flex-col gap-1 p-4 border-b last:border-b-0 border-white-100/10">
                     <div className="flex items-center gap-2">
                       <p className="w-14 text-xs text-secondary-600">Ov. {goalsOutcome[key as keyof typeof goalsOutcome]}</p>
-                      <div style={{ width: `calc(${value.over}% - 40px)` }} className="w-full min-w-[60px] h-[25px] flex items-center justify-end border border-highlight-400 bg-highlight-400/10 px-2">
+                      <div style={{ width: `calc(${value.over}% - 60px)` }} className="w-full min-w-[60px] h-[25px] flex items-center justify-end border border-highlight-400 bg-highlight-400/10 px-2">
                         <p className="text-highlight-400 font-semibold text-xs">{value.over?.toFixed(2)} <span className="font-normal">%</span></p>
                       </div>
                     </div>
                     <div className="flex items-center gap-2">
                       <p className="w-14 text-xs text-secondary-600">Un. {goalsOutcome[key as keyof typeof goalsOutcome]}</p>
-                      <div style={{ width: `calc(${value.under}% - 40px)` }} className="w-full min-w-[60px]  h-[25px] flex items-center justify-end border border-highlight-600 bg-highlight-600/10 px-2">
+                      <div style={{ width: `calc(${value.under}% - 60px)` }} className="w-full min-w-[60px]  h-[25px] flex items-center justify-end border border-highlight-600 bg-highlight-600/10 px-2">
                         <p className="text-highlight-600 font-semibold text-xs">{value.under?.toFixed(2)} <span className="font-normal">%</span></p>
                       </div>
                     </div>
