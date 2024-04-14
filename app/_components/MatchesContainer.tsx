66b5c913a436175555723243a5ed84d2062b8dc4
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-04-14 05:42:00 +0100

    made some styling changes and changed import path for 'Match' type

--- app/_components/MatchesContainer.tsx
+++ app/_components/MatchesContainer.tsx
@@ -1,10 +1,10 @@
-import { Match } from '@/types/match.type';
+import { Match } from '@/types/global.type';
 import React from 'react';
 import MatchCard from './MatchCard';
 
 function MatchesContainer({ matches }: { matches: Match[] }) {
     return (
-        <ul className='flex flex-col rounded-md border border-white-100/5 overflow-hidden pb-[-1px]'>
+        <ul className='flex flex-col rounded-md border border-white-100/5 overflow-hidden'>
             {
                 matches.map((match) => <MatchCard {...match} />)
             }
