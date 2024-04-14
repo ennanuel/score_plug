96d0ee0d1ae21ab195e6b520a8a497d447df1162
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-04-14 05:39:05 +0100

    chagned the import path for 'Competition' type

--- app/_components/CompetitionWithMatches.tsx
+++ app/_components/CompetitionWithMatches.tsx
@@ -3,13 +3,13 @@
 import { MdStar, MdStarOutline } from 'react-icons/md';
 import Link from "next/link";
 import Image from "next/image";
-import { MatchCard, MatchesContainer } from '.';
-import { Competition } from '@/types/competition.type';
+import { MatchesContainer } from '.';
+import { Competition } from '@/types/global.type';
 
 const CompetitionWithMatches = ({ _id, name, emblem, area, matches, isFavorite }: Competition) => {
     return (
-        <div className='mt-4 flex flex-col gap-3'>
-            <div className='flex items-center gap-3 p-2'>
+        <div className='mt-4 flex flex-col gap-2'>
+            <div className='flex items-center gap-3 border-t border-white-100/5 p-2'>
                 <Image src={emblem || String(process.env.NEXT_IMAGE_URI)} width={40} height={40} alt={name} className="aspect-square object-contain" />
                 <Link href={`/competition/${_id}`} className="flex-1 flex flex-col">
                     <span className="font-semibold text-secondary-500 text-sm">{name}</span>
