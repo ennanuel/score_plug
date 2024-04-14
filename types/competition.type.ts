211dddfc5fb1b81a8696c09a15ebd6f13e99c221
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-04-14 05:28:09 +0100

    moved 'Competition' type

--- types/competition.type.ts
+++ types/competition.type.ts
@@ -1,6 +1,4 @@
-import { StaticImport } from "next/dist/shared/lib/get-img-props";
-import { Match } from "./match.type";
-import { Team } from "./team.type";
+import { Team } from "./global.type";
 
 export type CompetitionTable = {
     position: number;
@@ -21,32 +19,4 @@ export type CompetitionStandings = {
     type: string;
     group: string;
     table: CompetitionTable[];
-};
-
-export type Competition = {
-    _id: number;
-    code: string;
-    area: {
-        name: string;
-        flag: string | StaticImport;
-    };
-    name: string;
-    type: string;
-    emblem: string | StaticImport;
-    currentSeason: {
-        startDate: string;
-        endDate: string;
-        currentMatchday: number;
-        winner: number;
-    };
-    hasLiveMatch: boolean;
-    noOfMatches: number;
-    matches: Match[];
-    recentMatches: {
-        matches: number;
-        hasLiveMatch: boolean;
-    };
-    isFavorite: boolean;
-    teams: Team[];
-    standings: CompetitionStandings[];
 };
