607057ec6897f3b1027d29a25a14ad78a66d293e
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-04-14 05:45:00 +0100

    implemented graphql query to get team data and changed 'Team' type import path

--- app/_components/TeamDetailsHeader.tsx
+++ app/_components/TeamDetailsHeader.tsx
@@ -1,15 +1,41 @@
+"use client";
+
 import { FaAngleLeft } from 'react-icons/fa';
 import { MdStarOutline } from 'react-icons/md';
 import Image from "next/image";
-import barcelonaCrest from "../_assets/barcelona_crest.png";
+
 import { useParams } from 'next/navigation';
 import AltHeader from './AltHeader';
 import { getHeaderLinks } from '../_utils/link';
 import { TEAM_LINKS } from '../_assets/constants/team';
+import { gql, useQuery } from '@apollo/client';
+import { Team } from '@/types/global.type';
+import LoadingMessage from './LoadingMessage';
+import ErrorMessage from './ErrorMessage';
+
+const QUERY = gql`
+  query GetTeam($id: ID!) {
+    team(id: $id) {
+      _id
+      name
+      crest
+      area {
+        name
+        flag
+      }
+    }
+  }
+`
 
 const TeamDetailsHeader = () => {
   const { id } = useParams<{ id: string }>();
   const links = getHeaderLinks({ path: 'team', id, links: TEAM_LINKS });
+
+  const { loading, error, data } = useQuery<{ team: Team }>(QUERY, { variables: { id } });
+  
+  if (loading) return <LoadingMessage />;
+  else if (error) return <ErrorMessage />;
+  else if (!data) return <div>Nothing was found</div>;
 
   return (
     <>
@@ -17,10 +43,13 @@ const TeamDetailsHeader = () => {
         <button className="h-8 aspect-square rounded-full hover:bg-secondary-400/10">
           <FaAngleLeft size={20} />
         </button>
-        <Image src={barcelonaCrest} alt="Clug Emblem" width={80} />
+        <Image src={data.team.crest || String(process.env.NEXT_IMAGE_URL)} alt="Clug Emblem" width={80} className="aspect-square object-contain" />
         <div className="flex-1 flex-col">
-          <p className="font-bold">FC Barcelona</p>
-          <p className="text-sm text-secondary-600">Spain</p>
+          <h2 className="font-bold text-lg">{data.team.name}</h2>
+          <div className="flex items-center gap-2">
+            <Image src={data.team.area.flag || String(process.env.NEXT_IMAGE_URL)} alt={data.team.name + ' crest'} width={20} className='aspect-square object-contain' />
+            <p className="text-sm text-secondary-600">{data.team.area.name}</p>
+          </div>
         </div>
         <MdStarOutline size={20} />
       </div>
