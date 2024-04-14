1e6bbfa29f17de467ba1b8ec8b6540fb20653a63
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-04-14 05:44:10 +0100

    Made some style changes and changd the import path for 'Team' type

--- app/_components/TeamCard.tsx
+++ app/_components/TeamCard.tsx
@@ -1,10 +1,10 @@
-import { Team } from '@/types/team.type';
+import { Team } from '@/types/global.type';
 import Image from 'next/image';
 import Link from 'next/link';
 
 function TeamCard ({ _id, name, crest, hasOngoingMatch }: Team) {
     return (
-        <Link href={`/team/${_id}`} className={`relative flex flex-col items-center justify-center p-3 gap-3 bg-primary-600 rounded-md hover:bg-primary-500/80 border border-transparent ${hasOngoingMatch && 'border-highlight-400/50 hover:border-transparent'}`}>
+        <Link href={`/team/${_id}`} className={`relative flex flex-col items-center justify-center p-3 gap-3 bg-primary-600 rounded-sm hover:bg-primary-500/80 border border-transparent ${hasOngoingMatch && 'border-highlight-400/50 hover:border-transparent'}`}>
             {
                 hasOngoingMatch ?
                     <div className="absolute top-[-4px] right-[-4px] w-3 aspect-square rounded-full bg-highlight-500 border-2 border-primary-900" /> :
