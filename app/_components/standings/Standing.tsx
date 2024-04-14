60b389a22e21f015546917871217ba4cb3aa755e
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-04-14 05:34:55 +0100

    moved functions within use memo to '_utils' folder

--- app/_components/standings/Standing.tsx
+++ app/_components/standings/Standing.tsx
@@ -3,40 +3,17 @@
 import { useMemo } from "react";
 import { TeamStanding } from "@/types/team.type";
 import Image from "next/image";
+import { getTeamStandingStatus } from "@/app/_utils/competition";
+import { getTablePositionColor, getTableRowHighlightColors } from "@/app/_utils/colors";
 
 
 function Standing({ highlightedTeams, teamStanding, relegationPositions, topPositions, midPositions }: TeamStanding) {
-    const highlightTeam = useMemo(() => highlightedTeams.includes(teamStanding?.team?._id), []);
+    const highlightTeam = useMemo(() => highlightedTeams.includes(teamStanding.team?._id), []);
 
-    const teamPosition = useMemo(() => teamStanding.position <= topPositions ?
-        'TOP_TEAM' :
-        (teamStanding.position > topPositions && teamStanding.position <= midPositions) ?
-            'MID_TEAM' :
-            (teamStanding.position >= relegationPositions) ?
-                'LOW_TEAM' :
-                '',
-        []);
+    const teamPosition = useMemo(() => getTeamStandingStatus({ teamStanding, topPositions, midPositions, relegationPositions }), []);
 
-    const highlightColors = useMemo(() => highlightTeam ?
-        (
-            teamPosition === 'TOP_TEAM' ?
-                'bg-green-400/10 border-green-500' :
-                teamPosition === 'MID_TEAM' ?
-                    'bg-yellow-400/10 border-yellow-500' :
-                    teamPosition === 'LOW_TEAM' ? 'bg-red-400/10 border-red-500' :
-                        'bg-white-100/10 border-white-500'
-        ) :
-        'border-transparent',
-        []);
-
-    const positionColors = useMemo(() => teamPosition === 'TOP_TEAM' ?
-        'bg-green-500 text-primary-800' :
-        teamPosition === 'MID_TEAM' ?
-            'bg-yellow-500 text-primary-800' :
-            teamPosition === 'LOW_TEAM' ?
-                'bg-red-500 text-primary-800' :
-                'text-gray-500',
-        []);
+    const highlightColors = useMemo(() => getTableRowHighlightColors(highlightTeam, teamPosition), []);
+    const positionColors = useMemo(() => getTablePositionColor(teamPosition), []);
 
     return (
         <tr className={`text-xs text-center h-10 border-l-4 hover:bg-primary-500 ${highlightColors}`}>
@@ -46,7 +23,12 @@ function Standing({ highlightedTeams, teamStanding, relegationPositions, topPosi
                 </p>
             </td>
             <td className="text-left">
-                <Image src={teamStanding.team?.crest || String(process.env.NEXT_IMAGE_URL)} width={18} alt={teamStanding.team?.name} className="aspect-square object-contain float-left mr-2" />
+                <Image
+                    src={teamStanding.team?.crest || String(process.env.NEXT_IMAGE_URL)}
+                    width={18}
+                    alt={teamStanding.team?.name}
+                    className="aspect-square object-contain float-left mr-2"
+                />
                 <p>{teamStanding.team?.name}</p>
             </td>
             <td className="text-secondary-500">{teamStanding.playedGames}</td>
