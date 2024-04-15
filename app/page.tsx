c04ccd83c8f000b473d332c66cc3d7ae2341f0c4
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-04-15 14:04:51 +0100

    design changes

--- app/page.tsx
+++ app/page.tsx
@@ -54,11 +54,13 @@ function Home() {
   return (
     <main className="border border-secondary-900/50 bg-primary-600 flex flex-col gap-4">
       <div className="p-3">
-        <h2 className="font-bold">Today's Matches</h2>
         <div className="flex items-center justify-between px-3">
-          <div className="relative flex items-center justify-stretch h-[30px] gap-2 text-sm">
+          <h2 className="font-bold">Today's Matches</h2>
+          <MdStar />
+        </div>
+          <div className="mt-2 flex items-center justify-stretch h-[30px] gap-2 text-sm">
             <button onClick={showAllMatches} className={`relative flex justify-center items-center gap-2 h-full px-3 rounded-sm font-semibold ${status === "" ? 'text-orange-300 bg-orange-400/20': 'bg-black-900/50 text-orange-700'}`}>
-              {status === "" && <span className="block w-2 h-4 rounded-md bg-orange-300"></span>}
+              {status === "" && <span className="block w-1 h-4 rounded-md bg-orange-300"></span>}
               <span>All</span>
             </button>
             <button onClick={showLiveMatches} className={`relative flex justify-center items-center gap-2 h-full px-3 rounded-sm font-semibold ${status === "IN_PLAY" ? ' text-green-300 bg-green-400/20': 'bg-black-900/50 text-green-700'}`}>            
@@ -66,8 +68,6 @@ function Home() {
               <span>Live</span>
             </button>
           </div>
-          <MdStar />
-        </div>
       </div>
       <ul className="flex flex-col">
           {
