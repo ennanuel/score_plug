62343af0c2153541a63fef3f2a943cd50c2537a2
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-04-15 12:12:57 +0100

    added status as part of query

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import { useQuery, gql } from '@apollo/client';
 import { ErrorMessage, LoadingMessage } from './_components';
 
 const QUERY = gql`
-  query {
+  query GetActiveCompetitions($status: String!) {
     activeCompetitions {
       _id
       name
@@ -45,29 +45,31 @@ function Home() {
   const showLiveMatches = () => setStatus("IN_PLAY");
   const showAllMatches = () => setStatus("");
 
-  const { loading, error, data } = useQuery<{ activeCompetitions: Competition[] }>(QUERY);
+  const { loading, error, data } = useQuery<{ activeCompetitions: Competition[] }>(QUERY, { variables: { status } });
 
   if (loading) return <LoadingMessage />;
   else if (error) return <ErrorMessage />;
   else if (!data) return <div>Nothing was found</div>;
 
   return (
-    <main className="border border-secondary-900/50 bg-primary-600 flex flex-col gap-4 p-3">
-      <h2 className="font-bold">Today's Matches</h2>
-      <div className="flex items-center justify-between px-3">
-        <div className="relative flex items-center justify-stretch h-[30px] gap-2">
-          <button onClick={showAllMatches} className={`relative flex justify-center items-center gap-2 h-full px-3 rounded-sm font-semibold ${status === "" ? 'text-orange-300 bg-orange-400/20': 'bg-black-900/50 text-orange-700'}`}>
-            {status === "" && <span className="block w-2 aspect-square rounded-md text-sm bg-orange-300"></span>}
-            <span>All</span>
-          </button>
-          <button onClick={showLiveMatches} className={`relative flex justify-center items-center gap-2 h-full px-3 rounded-sm font-semibold ${status === "IN_PLAY" ? ' text-green-300 bg-green-400/20': 'bg-black-900/50 text-green-700'}`}>            
-            {status === "IN_PLAY" && <span className="block w-2 aspect-square rounded-md text-sm bg-green-600"></span>}
-            <span>Live</span>
-          </button>
+    <main className="border border-secondary-900/50 bg-primary-600 flex flex-col gap-4">
+      <div className="p-3">
+        <h2 className="font-bold">Today's Matches</h2>
+        <div className="flex items-center justify-between px-3">
+          <div className="relative flex items-center justify-stretch h-[30px] gap-2 text-sm">
+            <button onClick={showAllMatches} className={`relative flex justify-center items-center gap-2 h-full px-3 rounded-sm font-semibold ${status === "" ? 'text-orange-300 bg-orange-400/20': 'bg-black-900/50 text-orange-700'}`}>
+              {status === "" && <span className="block w-2 h-4 rounded-md bg-orange-300"></span>}
+              <span>All</span>
+            </button>
+            <button onClick={showLiveMatches} className={`relative flex justify-center items-center gap-2 h-full px-3 rounded-sm font-semibold ${status === "IN_PLAY" ? ' text-green-300 bg-green-400/20': 'bg-black-900/50 text-green-700'}`}>            
+              {status === "IN_PLAY" && <span className="block w-2 h-4 rounded-md bg-green-600"></span>}
+              <span>Live</span>
+            </button>
+          </div>
+          <MdStar />
         </div>
-        <MdStar />
       </div>
-      <ul className="flex flex-col gap-4">
+      <ul className="flex flex-col">
           {
             data?.activeCompetitions.map((competition, index) => <li key={index}><CompetitionWithMatches {...competition} /></li>)
           }
