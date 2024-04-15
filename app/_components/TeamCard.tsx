9166cfa428c3c6f0ecd5d03841e2d8853fb2a64a
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-04-15 11:49:59 +0100

    made some design changes

--- app/_components/TeamCard.tsx
+++ app/_components/TeamCard.tsx
@@ -7,7 +7,7 @@ function TeamCard ({ _id, name, crest, hasOngoingMatch }: Team) {
         <Link href={`/team/${_id}`} className={`relative flex flex-col items-center p-3 gap-4 bg-primary-600 rounded-sm hover:bg-primary-500/80 border border-transparent ${hasOngoingMatch && 'border-highlight-400/50 hover:border-transparent'}`}>
             {
                 hasOngoingMatch ?
-                    <div className="absolute top-[-4px] right-[-4px] w-3 aspect-square rounded-full bg-highlight-500 border-2 border-primary-900" /> :
+                    <div className="absolute top-[-4px] right-[-4px] w-2 aspect-square rounded-full bg-highlight-500 border-2 border-primary-900" /> :
                     null
             }
             <Image src={crest} alt={name} width={80} className="object-contain aspect-square" />
