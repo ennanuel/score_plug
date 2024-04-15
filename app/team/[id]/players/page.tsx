beb94cdb76e1147a0affdc40552d60045cde4cdf
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-04-15 11:53:25 +0100

    used dynamic player data

--- app/team/[id]/players/page.tsx
+++ app/team/[id]/players/page.tsx
@@ -1,7 +1,11 @@
 import { PLAYERS } from '@/app/_assets/constants/player';
+import { ErrorMessage, LoadingMessage } from '@/app/_components';
 import PlayerCard from '@/app/_components/PlayerCard';
-import { gql } from '@apollo/client';
+import { gql, useQuery } from '@apollo/client';
+import { useParams } from 'next/navigation';
 import React from 'react';
+
+import { Team } from "@/types/global.type";
 
 const QUERY = gql`
   query GetTeamPlayers($id: ID!) {
@@ -11,7 +15,7 @@ const QUERY = gql`
         name
         number
         position
-        
+
         country {
           name
           flag
@@ -22,6 +26,12 @@ const QUERY = gql`
 `;
 
 const TeamPlayers = () => {
+  const { id } = useParams();
+  const { loading, error, data } = useQuery<{ team: Team }>(QUERY, { variables: { id } });
+
+  if (loading) return <LoadingMessage />;
+  else if (error) return <ErrorMessage />;
+  else if (!data) return <div>Nothing was found!</div>;
   return (
     <ul className="grid grid-cols-5 gap-4 p-4">
       {
