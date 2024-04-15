2214eeef3ff62697a1ee0ec6297fa080f36fd0bd
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-04-15 12:13:22 +0100

    added borders

--- app/_components/CompetitionWithMatches.tsx
+++ app/_components/CompetitionWithMatches.tsx
@@ -8,7 +8,7 @@ import { Competition } from '@/types/global.type';
 
 const CompetitionWithMatches = ({ _id, name, emblem, area, matches, isFavorite }: Competition) => {
     return (
-        <div className='mt-4 flex flex-col gap-2'>
+        <div className='mt-4 flex flex-col gap-2 border-t border-white-100/10 p-2'>
             <div className='flex items-center gap-3 p-2'>
                 <Image src={emblem || String(process.env.NEXT_IMAGE_URI)} width={30} height={30} alt={name} className="aspect-square object-contain" />
                 <Link href={`/competition/${_id}`} className="flex-1 flex flex-col">
