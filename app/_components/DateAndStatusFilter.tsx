10255e87fc8ce662004b6f60f459e5015ef6f07e
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-04-15 20:31:13 +0100

    modified 'status' and 'date' logic

--- app/_components/DateAndStatusFilter.tsx
+++ app/_components/DateAndStatusFilter.tsx
@@ -1,58 +1,63 @@
 "use client"
 
 import { MdOutlineNotifications, MdKeyboardArrowRight, MdKeyboardArrowLeft } from 'react-icons/md';
-import { useState, useMemo, Suspense } from 'react';
+import { useState, useMemo, Suspense, Dispatch, SetStateAction, useEffect } from 'react';
 import { getMatchDates } from '../_utils/dateTime';
 import Link from 'next/link';
-import { usePathname, useSearchParams } from 'next/navigation';
 
-const MATCH_STATUS = [
-    { title: "All", value: "" },
-    { title: "Live", value: "in_play" },
-    { title: "Upcoming", value: "timed" },
-    { title: "Finished", value: "finished" }
-];
+const MATCH_STATUS = {
+    "all": "",
+    "live": "in_play",
+    "upcoming": "timed",
+    "finished": "finished"
+}
 
-function DateAndStatusFilter() {
-    const pathname = usePathname();
-    const params = useSearchParams();
-    const dateFilter = params.get("date") || (new Date()).toLocaleDateString();
-    const statusFilter = params.get("status") || "";
-    const status = MATCH_STATUS.find(status => status.value === statusFilter) || MATCH_STATUS[0];
+const DATES = getMatchDates();
+
+function DateAndStatusFilter({ setDate, setMatchStatus }: { setDate?: Dispatch<SetStateAction<string>>; setMatchStatus?: Dispatch<SetStateAction<string>>; }) {
+    const [status, setStatus] = useState<keyof typeof MATCH_STATUS>("all");
+    const [dateFilter, setDateFilter] = useState("");
+
+    const statusValue = useMemo(() => MATCH_STATUS[status], [status]);
 
     const [showStatus, setShowStatus] = useState(false);
 
-    const dates = useMemo(() => getMatchDates(), []);
+    useEffect(() => { 
+        if (!setDate || !setMatchStatus) return;
+        setDate(dateFilter);
+        setMatchStatus(statusValue);
+    }, [statusValue, dateFilter]);
 
     return (
-        <Suspense>
         <div className='border border-secondary-900/50 mt-2 py-2 px-3 flex justify-between items-center gap-3'>
             <button onClick={() => setShowStatus( prev => !prev )} className='py-[2px] px-2 pr-0 flex items-center justify-center rounded-[5px] bg-secondary-500 text-sm text-black-900 font-bold'>
-                <span>{status.title}</span>
+                <span>{status}</span>
                 { showStatus ? <MdKeyboardArrowLeft /> : <MdKeyboardArrowRight /> }
             </button>
             <div className="flex-1 relative overflow-clip">
                 <ul className={`w-full flex items-center justify-evenly gap-3 transition-[transform,opacity] ${showStatus && 'translate-x-[100%] opacity-0'}`}>
                     {
-                        dates.map(({ date, value }, index) => (
+                        DATES.map(({ date, value }, index) => (
                             <li key={index}>
-                                <Link href={`${pathname}?date=${value}`} className={`flex flex-col items-center justify-center ${dateFilter === value ? 'text-highlight-400' : 'text-secondary-600'}`}>
+                                <button onClick={() => setDateFilter(value)} className={`flex flex-col items-center justify-center ${dateFilter === value ? 'text-highlight-400' : 'text-secondary-600'}`}>
                                     <span className="text-xs font-semibold uppercase">{date.dayOfWeek}</span>
                                     <span className='text-[.7em] uppercase'>{date.day} {date.month}</span>
-                                </Link>
+                                </button>
                             </li>
                         ))
                     }
                 </ul>
                 <ul className={`absolute top-0 left-0 w-full h-full flex items-center justify-start gap-3 transition-[transform,opacity] ${!showStatus && 'translate-x-[-100%] opacity-0'}`}>
                     {
-                        MATCH_STATUS.filter(status => status.value !== statusFilter).map(({ title, value }, index) => (
-                            <li key={index}>
-                                <Link href={`${pathname}?status=${value}`} className="py-1 px-3 flex rounded-[5px] bg-secondary-900/50 text-sm text-white-600 font-semibold">
-                                    <span>{title}</span>
-                                </Link>
-                            </li>
-                        ))
+                        Object.keys(MATCH_STATUS)
+                            .filter((title) => title !== status)
+                            .map((title) => (
+                                <li key={title}>
+                                    <button onClick={() => setStatus((title as keyof typeof MATCH_STATUS))} className="py-1 px-3 flex rounded-[5px] bg-secondary-900/50 text-sm text-white-600 font-semibold">
+                                        <span>{title}</span>
+                                    </button>
+                                </li>
+                            ))
                     }
                 </ul>
             </div>
@@ -60,7 +65,6 @@ function DateAndStatusFilter() {
                     <MdOutlineNotifications />
             </button>
         </div>
-        </Suspense>
     )
 };
 
