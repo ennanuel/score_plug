2b0218359d8022e55a7b89ea880b118ffb075f3d
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-04-21 08:43:00 +0100

    made styling changes

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -21,18 +21,18 @@ const Header = () => {
 
   return (
     <>
-      <div className="fixed top-0 left-0 px-3 h-[50px] z-[2] flex-1 font-semibold border-r border-secondary-900/50 bg-primary-900 text-white-100 flex items-center justify-start gap-3">
+      <div className="fixed top-0 left-0 pl-3 pr-6 h-[50px] z-[2] flex-1 font-semibold border-r border-b border-secondary-900/50 bg-primary-900 text-white-100 flex items-center justify-start gap-3">
         <PiPlug size={25} /> 
         <span>ScorePlug</span>
       </div>
       <div className="flex items-stretch gap-4 px-4 h-[50px] bg-primary-900 border-b border-secondary-900/50">
         <div className="flex-1 opacity-0"></div>
-        <div className='overflow-clip flex-2 flex items-stretch justify-start gap-2 border-r border-secondary-900/50 hover:bg-secondary-900/50'>
+        <div className='overflow-clip flex-2 flex items-stretch justify-start gap-2 border-x border-secondary-900/50 hover:bg-secondary-900/50'>
           <label htmlFor="search" className="text-white-500 flex items-center justify-center w-[50px] border-r border-secondary-900/50">
             <MdSearch />
           </label>
           <input 
-            className="text-sm flex-1 bg-transparent border-none outline-none text-white-600 focus:text-white-200 h-[35px] pr-2" 
+            className="text-sm flex-1 bg-transparent border-none outline-none text-white-600 focus:text-white-200 pr-2" 
             type="text" 
             id="search" 
             placeholder="Barcelona v Chelsea" 
@@ -48,8 +48,8 @@ const Header = () => {
           </li>
         </ul>
       </div>
-      <nav className="sticky z-[1] h-[50px] top-0 bg-primary-900">
-        <ul className={`flex items-center justify-start transition-transform w-fit ${scrolled && "lg:translate-x-[130px]"}`}>
+      <nav className="sticky z-[1] h-[50px] top-0 border-b border-secondary-900/50 bg-primary-900">
+        <ul className={`flex items-center justify-start transition-transform w-fit ${scrolled && "lg:translate-x-[180px]"}`}>
           {
             NAV_LINKS.map((navLink, index) => (
               <li className="border-r first:border-x border-secondary-900/50"><NavLink key={index} {...navLink} /></li>
--- app/_components/NavLink.tsx
+++ app/_components/NavLink.tsx
@@ -7,7 +7,7 @@ const NavLink = ({ href, title, Icon, ActiveIcon, alert }: { href: string; title
     const active = pathname === href;
 
     return (
-        <Link href={href} className={`h-[50px] flex items-center justify-center gap-2 px-4 text-sm ${active ? 'bg-secondary-900/50 text-secondary-100 font-semibold' : 'bg-transparent text-secondary-700 hover:bg-primary-900/30 hover:text-secondary-500'}`}>
+        <Link href={href} className={`h-[50px] flex items-center justify-center gap-2 px-6 text-sm ${active ? 'bg-secondary-900/50 text-secondary-100 font-semibold' : 'bg-transparent text-secondary-700 hover:bg-primary-900/30 hover:text-secondary-500'}`}>
             {active ? <ActiveIcon size={15} /> : <Icon size={15} />}
             <span>{title}</span>
             {alert && <div className="h-4 w-1 rounded-md bg-highlight-400" />}
