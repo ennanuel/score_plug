cdac4d2470d5b978f43eacba7c87265343fe8df1
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-04-21 08:35:44 +0100

    Made styling changes

--- app/_components/NavLink.tsx
+++ app/_components/NavLink.tsx
@@ -7,10 +7,10 @@ const NavLink = ({ href, title, Icon, ActiveIcon, alert }: { href: string; title
     const active = pathname === href;
 
     return (
-        <Link href={href} className={`relative h-[30px] flex items-center justify-center gap-2 px-3 rounded-sm text-sm ${active ? 'bg-secondary-100 text-black-900 font-semibold' : 'bg-primary-100/10 text-secondary-600 hover:bg-primary-200/20 hover:text-white-200'}`}>
+        <Link href={href} className={`h-[50px] flex items-center justify-center gap-2 px-4 text-sm ${active ? 'bg-secondary-900/50 text-secondary-100 font-semibold' : 'bg-transparent text-secondary-700 hover:bg-primary-900/30 hover:text-secondary-500'}`}>
             {active ? <ActiveIcon size={15} /> : <Icon size={15} />}
             <span>{title}</span>
-            {alert && <div className="absolute top-[-3px] left-[-3px] h-3 aspect-square rounded-full bg-highlight-400 border-2 border-primary-600" />}
+            {alert && <div className="h-4 w-1 rounded-md bg-highlight-400" />}
         </Link>
     )
 };
