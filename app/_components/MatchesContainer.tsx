8809721da4cec09241ff96614a9fc5bf88170248
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-04-27 18:21:58 +0100

    editted socket matches update logic

--- app/_components/MatchesContainer.tsx
+++ app/_components/MatchesContainer.tsx
@@ -7,7 +7,17 @@ import { SocketContext } from '../SocketContext';
 
 function MatchesContainer({ matches }: { matches: Match[] }) {
     const { socketData } = useContext(SocketContext);
-    const matchesToView = useMemo(() => matches.map(match => ({ ...match, ...(socketData.matches[match._id] || {}) })), [socketData]);
+    const matchesToView = useMemo(() => matches.map(match => {
+        const socketMatch = socketData.matches[match._id];
+        const updatedMatch = {
+            ...match,
+            status: socketMatch.status || match.status,
+            minute: socketMatch.minute || match.minute,
+            score: socketMatch.score || match.score,
+            timeRemaining: socketMatch.timeRemaining || match.timeRemaining
+        }
+        return updatedMatch;
+    }), [socketData]);
 
     return (
         <ul className='flex flex-col rounded-md border border-white-100/5 overflow-hidden'>
