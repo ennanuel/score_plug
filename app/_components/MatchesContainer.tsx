018f642f1613bc6e62d1d7fb6601fe641f6501f0
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-04-27 18:08:59 +0100

    Made 'MatchesContainer' component to update match data dynamically with socket

--- app/_components/MatchesContainer.tsx
+++ app/_components/MatchesContainer.tsx
@@ -1,12 +1,16 @@
 import { Match } from '@/types/global.type';
-import React from 'react';
+import React, { useContext, useMemo } from 'react';
 import MatchCard from './MatchCard';
+import { SocketContext } from '../SocketContext';
 
 function MatchesContainer({ matches }: { matches: Match[] }) {
+    const { socketData } = useContext(SocketContext);
+    const matchesToView = useMemo(() => matches.map(match => ({ ...match, ...(socketData.matches[match._id] || {}) })), [socketData]);
+
     return (
         <ul className='flex flex-col rounded-md border border-white-100/5 overflow-hidden'>
             {
-                matches.map((match) => <MatchCard {...match} />)
+                matchesToView.map((match) => <MatchCard {...match} />)
             }
         </ul>
     )
