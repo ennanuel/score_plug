28d81a54f96a127c23217d7dc28e01bb47707331
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-24 19:06:04 +0100

    Made design modifications

--- app/_components/loading/CompetitionLoading.tsx
+++ app/_components/loading/CompetitionLoading.tsx
@@ -16,8 +16,13 @@ const CompetitionLoading = ({ size, small }: { size: number, small?: boolean }) 
                         className={`border-b last:border-none border-secondary-900/50 ${small ? 'p-2' : 'p-3'} flex items-center gap-2`}
                     >
                         <span className={`${small ? 'w-6 h-6' : 'w-8 h-8'}  ml-2 rounded-full bg-secondary-900/50`}></span>
-                        <span className='animate-loadopacity w-full max-w-[160px] h-4 rounded-sm bg-secondary-900/50'></span>
-                        {!small ? <span className="w-4 h-4 rounded-full bg-secondary-900/50"></span> : null}
+                        <div className="flex-1 flex flex-col gap-1">
+                            <div className="w-full flex justify-between items-center gap-1">
+                                <span className='animate-loadopacity w-full max-w-[160px] h-4 rounded-sm bg-secondary-900/50'></span>
+                                {!small ? <span className="w-2 h-2 rounded-full bg-secondary-900/50"></span> : null}
+                            </div>
+                            <span className="animate-loadopacity w-full max-w-[100px] h-2 rounded-sm bg-secondary-900/50"></span>
+                        </div>
                     </li>
                 ))
             }
