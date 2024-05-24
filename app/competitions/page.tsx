abd978b317866dc5fa893adc8e630c697e2aeb21
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-24 19:02:07 +0100

    Refactored code

--- app/competitions/page.tsx
+++ app/competitions/page.tsx
@@ -39,20 +39,21 @@ const Competitions = () => {
         recentMatches: { ...competition.recentMatches, hasLiveMatch: socketData.competitions.includes(competition._id) }
     })), [socketData, data])
 
-    if (error) return <ErrorMessage />;
-
     return (
         <div className="p-4">
             <div className="flex items-center gap-4 justify-between">
                 <h1 className="font-bold">Leagues and Competitions</h1>
                 <MdStarOutline size={20} />
             </div>
-        
             {
                 loading ?
-                    <div className="mt-6">
-                        <CompetitionLoading size={6} />
-                    </div> :
+                    error ?
+                        <div className="mt-6">
+                            <ErrorMessage /> 
+                        </div>:
+                        <div className="mt-6">
+                            <CompetitionLoading size={6} />
+                        </div> :
                     <ul className="flex flex-col mt-6 border border-secondary-900/50 rounded-md overflow-hidden">
                         {
                             competitions?.map((competition, index) => <CompetitionCard {...competition} key={index} />)
