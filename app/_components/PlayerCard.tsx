a02485cc956f091e73732d517e2ee81b140a66c7
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-27 13:17:43 +0100

    made changes to design and layout

--- app/_components/PlayerCard.tsx
+++ app/_components/PlayerCard.tsx
@@ -1,14 +1,13 @@
 import { Player } from '@/types/player.type';
 import Image from "next/image";
 
-const PlayerCard = ({ name, country }: Player) => {
+const PlayerCard = ({ name, nationality, shirtNumber }: Player) => {
     return (
-        <div className="flex flex-col items-center justify-center p-3 gap-3 bg-primary-600 rounded-md hover:bg-primary-500">
-            <div className="absolute top-1 right-1">
-                <Image src={country.flag} alt={name} width={40} className='object-contain' />
+        <div className="flex flex-col items-center justify-center p-3 gap-2 bg-primary-600 rounded-md hover:bg-primary-500">
+            <div className="w-full aspect-square rounded-full bg-white-900/50 relative font-bold text-white text-2xl">
+                {shirtNumber}
             </div>
-            <div className="w-full aspect-square rounded-full bg-white-900/50 relative">
-            </div>
+            <p className="text-[.65rem] text-secondary-700 mt-2">{nationality}</p>
             <p className="w-full font-semibold text-secondary-600 truncate text-sm">{name}</p>
         </div>
     )
