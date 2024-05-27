9df1634dd8322bf5361343e49b5601b0ca175ed8
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-27 15:20:52 +0100

    made design and layout changes

--- app/team/[id]/players/page.tsx
+++ app/team/[id]/players/page.tsx
@@ -7,6 +7,7 @@ import { useParams } from 'next/navigation';
 import React from 'react';
 
 import { Team } from "@/types/global.type";
+import { CompetitionLoading } from '@/app/_components/loading';
 
 const QUERY = gql`
   query GetTeamPlayers($id: ID!) {
@@ -30,20 +31,14 @@ const TeamPlayers = () => {
   const { id } = useParams();
   const { loading, error, data } = useQuery<{ team: Team }>(QUERY, { variables: { id } });
 
-  if (error) return <ErrorMessage />;
+  if (loading) return <CompetitionLoading size={11} />
+  else if (error) return <ErrorMessage />;
   else if (!data) return <NothingWasFound />;
   
   return (
-    <ul className="grid grid-cols-5 gap-4 p-4">
+    <ul className="flex flex-col border border-secondary-900/50 rounded-md overflow-clip">
       {
-        loading ?
-          [1, 2, 3, 4, 5].map((num, index) => (
-            <li key={index} className="flex flex-col gap-2 p-2 rounded-md bg-secondary-900/40">
-              <div className="aspect-square rounded-full bg-secondary-900/50 animate-loadopacity"></div>
-              <div className="h-4 max-w-[80%] rounded-md bg-secondary-900/50 animate-loadopacity"></div>
-            </li>
-          )) :
-          data.team.squad.map((player, index) => (<li key={index}><PlayerCard {...player} /></li>))
+        data.team.squad.map((player, index) => (<li key={index}><PlayerCard {...player} /></li>))
       }
     </ul>
   )
