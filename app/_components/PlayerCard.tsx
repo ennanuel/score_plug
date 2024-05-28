c4b65964a240b2b7417f563e6063042043a9bf6c
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-28 02:12:45 +0100

    ...

--- app/_components/PlayerCard.tsx
+++ app/_components/PlayerCard.tsx
@@ -1,11 +1,6 @@
-"use client";
-
 import { Player } from '@/types/player.type';
-import { useMemo } from 'react';
 
-const PlayerCard = ({ name, nationality, position, dateOfBirth }: Player) => {
-    const age = useMemo(() => (new Date()).getFullYear() - (new Date(dateOfBirth)).getFullYear(), []);
-
+const PlayerCard = ({ name, nationality, position }: Player) => {
     return (
         <div className="flex items-center p-2 gap-3 hover:bg-secondary-900/50 border-b last:border-transparent border-secondary-900/50">
             <div className="h-[30px] aspect-square rounded-full bg-white-900/50 relative font-bold text-white text-2xl" />
@@ -16,7 +11,6 @@ const PlayerCard = ({ name, nationality, position, dateOfBirth }: Player) => {
                 </div>
                 <div className="flex items-center justify-between gap-4">
                     <p className="w-full text-secondary-600 truncate text-xs font-semibold">{position}</p>
-                    <p className="w-full text-secondary-600 truncate text-xs">{age + age === 1 ? '' : 's'}</p>
                 </div>
             </div>
         </div>
