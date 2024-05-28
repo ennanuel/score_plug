966b84c5d235fe05d5bcbe669cc8306a7b975125
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-28 03:00:55 +0100

    wait...

--- app/_components/FormBox.tsx
+++ app/_components/FormBox.tsx
@@ -8,9 +8,9 @@ const FormBox = ({ outcome, teamCrest }: { outcome: string, teamCrest: string })
     const colors = useMemo(() => getTeamFormColors(outcome), []);
     
     return (
-        <div className={`${colors} h-[40px] px-3 aspect-square rounded-md border flex items-center justify-center gap-2`}>
-            <Image src={teamCrest} width={20} alt="" className="object-contain aspect-square rounded-md" />
-            <hr className="border-none border-l border-secondary-900/50 h-[20px] w-0 outline-none" />
+        <div className={`${colors} px-2 h-[30px] flex-col rounded-md border flex items-center justify-center gap-1`}>
+            <Image src={teamCrest} width={25} alt="" className="object-contain aspect-square rounded-md" />
+            <hr className="border-none bg-secondary-900/50 h-[20px] w-[1px] outline-none" />
             <span className="text-sm font-bold">{outcome}</span>
         </div>
     )
