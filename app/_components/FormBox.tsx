70a163c117f893b9ab51665a93476eb029b1e80f
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-28 19:13:13 +0100

    Changed height of image

--- app/_components/FormBox.tsx
+++ app/_components/FormBox.tsx
@@ -4,12 +4,12 @@ import React, { useMemo } from 'react';
 import Image from "next/image";
 import { getTeamFormColors } from '../_utils/colors';
 
-const FormBox = ({ outcome, teamCrest }: { outcome: string, teamCrest: string }) => {
+function FormBox({ outcome, teamCrest }: { outcome: string, teamCrest: string }) {
     const colors = useMemo(() => getTeamFormColors(outcome), []);
     
     return (
-        <div className={`${colors} px-2 h-[30px] rounded-md border flex items-center justify-center gap-2`}>
-            <Image src={teamCrest} height={24} width={24} alt="" className="object-contain" />
+        <div className={`${colors} px-2 h-[30px] border flex items-center justify-center gap-2`}>
+            <Image src={teamCrest} height={20} width={24} alt="" className="object-contain" />
             <span className="text-sm font-bold">{outcome}</span>
         </div>
     )
