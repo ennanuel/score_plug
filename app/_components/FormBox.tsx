aef846418a84602bfc71b6590ecbaccb5d112d5b
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-28 02:50:35 +0100

    made style changes

--- app/_components/FormBox.tsx
+++ app/_components/FormBox.tsx
@@ -8,8 +8,9 @@ const FormBox = ({ outcome, teamCrest }: { outcome: string, teamCrest: string })
     const colors = useMemo(() => getTeamFormColors(outcome), []);
     
     return (
-        <div className={`${colors} py-1 px-2 aspect-square rounded-md border flex items-center justify-center gap-2 font-semibold`}>
-            <Image src={teamCrest} width={10} height={10} alt="" className="object-contain" />
+        <div className={`${colors} h-[40px] px-3 aspect-square rounded-md border flex items-center justify-center gap-2`}>
+            <Image src={teamCrest} width={20} alt="" className="object-contain aspect-square rounded-md" />
+            <hr className="border-none border-l border-secondary-900/50 h-[20px] w-0 outline-none" />
             <span className="text-sm font-bold">{outcome}</span>
         </div>
     )
