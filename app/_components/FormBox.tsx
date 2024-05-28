40604bba1578180da0afe59ae568106ae5fa2c75
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-28 03:13:03 +0100

    increased gap

--- app/_components/FormBox.tsx
+++ app/_components/FormBox.tsx
@@ -8,7 +8,7 @@ const FormBox = ({ outcome, teamCrest }: { outcome: string, teamCrest: string })
     const colors = useMemo(() => getTeamFormColors(outcome), []);
     
     return (
-        <div className={`${colors} px-2 h-[30px] rounded-md border flex items-center justify-center gap-1`}>
+        <div className={`${colors} px-2 h-[30px] rounded-md border flex items-center justify-center gap-2`}>
             <Image src={teamCrest} width={24} alt="" className="object-contain aspect-square rounded-md" />
             <hr className="border-none bg-secondary-900/50 h-[20px] w-[1px] outline-none" />
             <span className="text-sm font-bold">{outcome}</span>
