f8d54a1310df7f32d0c3bc324add4e23ffcaad98
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-05-28 01:29:20 +0100

    made style changes to border

--- app/_components/loading/CompetitionWithMatchesLoading.tsx
+++ app/_components/loading/CompetitionWithMatchesLoading.tsx
@@ -12,7 +12,11 @@ const CompetitionWithMatchesLoading = ({ size }: { size: number }) => {
         <ul className="flex flex-col gap-2">
             {
                 competitions.map((key) => (
-                    <li key={key} style={{ '--delay': `${key * 100}ms` } as React.CSSProperties} className='flex flex-col gap-4 border-t border-white-100/10 p-4'>
+                    <li
+                        key={key}
+                        style={{ '--delay': `${key * 100}ms` } as React.CSSProperties}
+                        className='flex flex-col gap-4 border-b border-white-100/10 last:border-transparent p-4'
+                    >
                         <div className='flex items-center gap-3 px-2'>
                             <span className="w-[40px] h-[40px] rounded-full bg-secondary-900/50" />
                             <div className="flex-1 flex flex-col gap-2">
