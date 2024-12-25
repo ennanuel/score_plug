f6ddebde03776087a2b2e8be29a49a8783412017
Eunoia <ezemaemmanuel6@gmail.com>
2024-12-25 16:53:24 +0100

    Changed left sidebar styling

--- app/_components/Leftbar.tsx
+++ app/_components/Leftbar.tsx
@@ -9,6 +9,8 @@ import { Competition, Team } from "@/types/global.type";
 import { loadImage } from "../_utils/competition";
 import { CompetitionLoading } from "./loading";
 
+import { BiCaretDown } from "react-icons/bi";
+
 const QUERY = gql`
   query GetTopCompetitionsAndTeams {
     topCompetitions {
@@ -23,15 +25,6 @@ const QUERY = gql`
         hasLiveMatch
       }
     }
-
-    teams(limit: 10) {
-      teams {
-        _id
-        name
-        crest
-        hasOngoingMatch
-      }
-    }
   }
 `
 
@@ -41,55 +34,36 @@ const Leftbar = () => {
   if (error) return <ErrorMessage />;
 
   return (
-    <div className="col-span-1 sticky top-[50px] flex flex-col gap-2">
-      <div className="border-b border-secondary-900/50 p-3">
-        <div className="flex justify-between items-center mb-2">
-          <h2 className="font-bold text-white-300">Top Leagues</h2>
-          <Link href="/competitions" className="text-xs text-secondary-700 hover:text-secondary-500">More</Link>
-        </div>
+    <div className="col-span-1 sticky top-[50px] flex flex-col gap-4">
+      <div className="pb-2 pt-4 rounded-lg border border-white-100/10 bg-white-100/5 flex flex-col gap-4">
+        <h2 className="font-semibold text-white-300 text-sm px-4">Top Leagues</h2>
         {
           loading ?
             <CompetitionLoading size={5} small={true} /> :
-            <ul className="flex mt-4 flex-col rounded-md overflow-hidden border border-secondary-900/50">
+            <ul className="flex flex-col">
               {
-                data?.topCompetitions?.slice(0, 6)?.map((competition) => (
-                  <li key={competition._id} className="border-b border-secondary-900/50 last:border-none">
-                    <Link href={`/competition/${competition._id}`} className="flex items-center justify-between py-2 px-3 gap-3">
-                      <Image loader={loadImage} src={competition.emblem || String(process.env.NEXT_IMAGE_URL)} width={25} height={25} alt={competition.name} className="aspect-square object-contain" />
-                      {competition.recentMatches.hasLiveMatch ? <span className="mr-[-5px] bg-highlight-500 text-xs w-[3px] h-[10px] rounded-md"></span> : null}
-                      <span className={`text-sm font-semibold flex-1 ${competition.recentMatches.hasLiveMatch ? 'text-highlight-500' : 'text-secondary-600'}`}>{competition.name}</span>
-                      <span className="text-secondary-700 text-xs">{competition.area.name}</span>
-                    </Link>
-                  </li>
-                ))
+                data
+                  ?.topCompetitions
+                  ?.slice(0, 6)
+                  ?.map((competition) => (
+                    <li key={competition._id} className="">
+                      <Link href={`/competition/${competition._id}`} className="flex items-center h-9 px-4 gap-4 hover:bg-white-100/10">
+                        <Image loader={loadImage} src={competition.emblem || String(process.env.NEXT_IMAGE_URL)} width={20} height={20} alt={competition.name} className="w-4 aspect-square object-contain" />
+                        <span className={`text-xs ${competition.recentMatches.hasLiveMatch ? 'text-yellow-500' : 'text-white-600'}`}>{competition.name}</span>
+                        {competition.recentMatches.hasLiveMatch ? <span className="-ml-2 bg-yellow-500 text-xs w-1 aspect-square rounded-md"></span> : null}
+                      </Link>
+                    </li>
+                  ))
               }
             </ul>
         }
       </div>
 
-      
-      <div className="p-3">
-        <div className="flex justify-between items-center mb-2">
-          <h2 className="font-bold text-white-300">Top Teams</h2>
-          <span className="text-xs text-secondary-700 hover:text-secondary-500">More</span>
-        </div>
-        {
-          loading ?
-            <CompetitionLoading size={8} small={true} /> :
-            <ul className="flex mt-4 flex-col rounded-md overflow-hidden border border-secondary-900/50">
-              {
-                data?.teams?.teams?.map((team) => (
-                  <li key={team._id} className="border-b border-secondary-900/50 last:border-nond">
-                    <Link href={`/team/${team._id}`} className="flex items-center justify-between py-2 px-3 gap-3 hover:bg-secondary-900/50">
-                      <Image src={team.crest || String(process.env.NEXT_IMAGE_URL)} width={25} height={25} alt={team.name} className="aspect-square object-contain" />
-                      <span className="text-sm font-semibold text-secondary-600 flex-1">{team.name}</span>
-                      {team.hasOngoingMatch ? <span className="w-2 h-2 aspect-square rounded-full bg-secondary-500" /> : null}
-                    </Link>
-                  </li>
-                ))
-              }
-            </ul>
-        }
+      <div className="rounded-lg border border-white-100/10 bg-white-100/5 flex flex-col">
+        <button className="text-white-500 hover:text-white-600 flex items-center gap-3 p-4">
+          <span className="font-semibold text-sm ">All Leagues</span>
+          <BiCaretDown size={14} />
+        </button>
       </div>
     </div>
   )
