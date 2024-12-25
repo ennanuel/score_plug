4aa1a7941e791bccfdf32abbd57dbf209191af8b
Eunoia <ezemaemmanuel6@gmail.com>
2024-12-25 16:52:28 +0100

    Changed styling of loading component

--- app/_components/loading/CompetitionLoading.tsx
+++ app/_components/loading/CompetitionLoading.tsx
@@ -1,28 +1,22 @@
-import React, { useMemo } from 'react'
+import React, { useMemo } from 'react';
 
 const CompetitionLoading = ({ size, small }: { size: number, small?: boolean }) => {
     const competitions = useMemo(() => {
         const competitions = [];
-        for (let i = 0; i < size; i++) competitions.push(i);
+        for (let i = 0; i < size; i++) competitions.push(`${Math.ceil(Math.random() * 80)}`);
         return competitions;
     }, []);
 
     return (
-        <ul className="flex flex-col border border-secondary-900/50 rounded-md overflow-hidden">
+        <ul className="">
             {
-                competitions.map((key) => (
+                competitions.map((width, index) => (
                     <li
-                        key={key} style={{ '--delay': `${key * 100}ms` } as React.CSSProperties}
-                        className={`border-b last:border-none border-secondary-900/50 ${small ? 'p-2' : 'p-3'} flex items-center gap-2`}
+                        key={index} style={{ '--delay': `${index * 100}ms` } as React.CSSProperties}
+                        className={`${small ? 'h-8' : 'h-12'} w-full flex items-center gap-2 px-4`}
                     >
-                        <span className={`${small ? 'w-6 h-6' : 'w-8 h-8'}  ml-2 rounded-full bg-secondary-900/50`}></span>
-                        <div className="flex-1 flex flex-col gap-1">
-                            <div className="w-full flex justify-between items-center gap-1">
-                                <span className='animate-loadopacity w-full max-w-[160px] h-4 rounded-sm bg-secondary-900/50'></span>
-                                {!small ? <span className="w-2 h-2 rounded-full bg-secondary-900/50"></span> : null}
-                            </div>
-                            <span className="animate-loadopacity w-full max-w-[100px] h-2 rounded-sm bg-secondary-900/50"></span>
-                        </div>
+                        <span className={`${small ? 'w-4' : 'w-8'} aspect-square rounded-full bg-white-100/10`}></span>
+                        <span style={{ maxWidth: `${width}%` }} className={`${small ? 'h-3' : 'h-4'} min-w-[30%] animate-loadopacity w-full rounded-sm bg-white-100/10`}></span>
                     </li>
                 ))
             }
