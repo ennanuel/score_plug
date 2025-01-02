9970fdc0f37deec91fcab4cf52fdb7ef15b62e48
Eunoia <ezemaemmanuel6@gmail.com>
2025-01-02 13:38:12 +0100

    Modified layout and design of left side bar

--- app/_components/Leftbar.tsx
+++ app/_components/Leftbar.tsx
@@ -35,8 +35,8 @@ const Leftbar = () => {
 
   return (
     <div className="col-span-1 sticky top-[50px] flex flex-col gap-4">
-      <div className="pb-2 pt-4 rounded-lg border border-white-100/10 bg-white-100/5 flex flex-col gap-4">
-        <h2 className="font-semibold text-white-300 text-sm px-4">Top Leagues</h2>
+      <div className="pb-2 pt-4 rounded-xl border border-transparent bg-white-100/10 flex flex-col gap-4">
+        <h2 className="font-semibold text-white-300 text-xs px-4">Top Leagues</h2>
         {
           loading ?
             <CompetitionLoading size={5} small={true} /> :
@@ -49,7 +49,7 @@ const Leftbar = () => {
                     <li key={competition._id} className="">
                       <Link href={`/competition/${competition._id}`} className="flex items-center h-9 px-4 gap-4 hover:bg-white-100/10">
                         <Image loader={loadImage} src={competition.emblem || String(process.env.NEXT_IMAGE_URL)} width={20} height={20} alt={competition.name} className="w-4 aspect-square object-contain" />
-                        <span className={`text-xs ${competition.recentMatches.hasLiveMatch ? 'text-yellow-500' : 'text-white-600'}`}>{competition.name}</span>
+                        <span className={`text-2xs ${competition.recentMatches.hasLiveMatch ? 'text-yellow-500' : 'text-white-600'}`}>{competition.name}</span>
                         {competition.recentMatches.hasLiveMatch ? <span className="-ml-2 bg-yellow-500 text-xs w-1 aspect-square rounded-md"></span> : null}
                       </Link>
                     </li>
@@ -59,9 +59,9 @@ const Leftbar = () => {
         }
       </div>
 
-      <div className="rounded-lg border border-white-100/10 bg-white-100/5 flex flex-col">
+      <div className="rounded-xl border border-transparent bg-white-100/10 flex flex-col">
         <button className="text-white-500 hover:text-white-600 flex items-center gap-3 p-4">
-          <span className="font-semibold text-sm ">All Leagues</span>
+          <span className="font-semibold text-xs ">All Leagues</span>
           <BiCaretDown size={14} />
         </button>
       </div>
