caccf2beb36286c6cae6030e0e392c110ee30586
Eunoia <ezemaemmanuel6@gmail.com>
2025-01-02 13:08:11 +0100

    Modified position field

--- types/player.type.ts
+++ types/player.type.ts
@@ -1,12 +1,19 @@
 
 export type Player = {
-    _id: number,
-    firstName: number,
-    lastName: string,
-    name: string,
-    position: string,
-    dateOfBirth: string,
-    nationality: string,
-    shirtNumber: number,
-    marketValue: number
+    _id: number;
+    firstName: number;
+    lastName: string;
+    name: string;
+    position: {
+        area: 'goalkeeper' | 'defence' | 'midfield' | 'offence';
+        specialty: string;
+    };
+    dateOfBirth: string;
+    age: number;
+    nationality: string;
+    shirtNumber: number;
+    marketValue: number;
+    teamCrest: string;
+    id: string | number;
+    type: string;
 }
