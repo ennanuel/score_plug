7ad80564733267d2333364e59ad10805acebb443
Eunoia <ezemaemmanuel6@gmail.com>
2025-01-02 13:40:26 +0100

    Modified layout and design of component

--- app/_components/CompetitionWithMatches.tsx
+++ app/_components/CompetitionWithMatches.tsx
@@ -1,27 +1,29 @@
 "use client";
 
-import { MdStar, MdStarOutline } from 'react-icons/md';
 import Link from "next/link";
 import Image from "next/image";
 import { MatchesContainer } from '.';
 import { Competition } from '@/types/global.type';
 import { loadImage } from '../_utils/competition';
 
-const CompetitionWithMatches = ({ _id, name, emblem, area, matches, isFavorite }: Competition) => {
+const CompetitionWithMatches = ({ _id, name, area, matches }: Competition) => {
     return (
-        <li className='flex flex-col gap-2 border-b border-secondary-900/50 last:border-transparent p-4'>
-            <div className='flex items-center gap-3 px-2'>
-                <Image src={emblem || String(process.env.NEXT_IMAGE_URI)} loader={loadImage} width={30} height={30} alt={name} className="aspect-square object-contain" />
-                <Link href={`/competition/${_id}`} className="flex-1 flex flex-col">
-                    <span className="font-semibold text-secondary-500 text-sm">{name}</span>
-                    <span className="text-[.7em] text-secondary-700">{area.name}</span>
-                </Link>
-                {
-                    isFavorite ?
-                        <MdStar size={20} /> :
-                        <MdStarOutline size={20} />
-                }
-            </div>
+        <li className='flex flex-col border border-transparent bg-white-100/10 rounded-xl overflow-hidden'>
+            <Link href={`/competition/${_id}`} className='flex items-center bg-white-100/5 hover:bg-white-100/10 gap-3 p-3 border-b border-transparent'>
+                <Image 
+                    src={area.flag || String(process.env.NEXT_IMAGE_URI)} 
+                    loader={loadImage} 
+                    width={16} 
+                    height={16} 
+                    alt={name} 
+                    className="aspect-square object-cover rounded-full" 
+                />
+                <div className="flex-1 flex gap-1 text-white-300 text-2xs font-semibold">
+                    <span className="">{name}</span>
+                    <span className="font-normal">-</span>
+                    <span className="">{area.name}</span>
+                </div>
+            </Link>
             <MatchesContainer matches={matches} />
         </li>
     )
