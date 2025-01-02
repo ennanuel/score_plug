59dcf28bb07573baee3713279614cf7397db1c03
Eunoia <ezemaemmanuel6@gmail.com>
2025-01-02 13:14:27 +0100

    Modified page layout and design and added mroe fields in graphql query

--- app/team/[id]/players/page.tsx
+++ app/team/[id]/players/page.tsx
@@ -1,50 +1,83 @@
 "use client";
 
-import { ErrorMessage, NothingWasFound } from '@/app/_components';
-import PlayerCard from '@/app/_components/PlayerCard';
 import { gql, useQuery } from '@apollo/client';
 import { useParams } from 'next/navigation';
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import { Team } from "@/types/global.type";
-import { PlayerLoading } from '@/app/_components/loading';
+import { IoPerson } from 'react-icons/io5';
 
 const QUERY = gql`
   query GetTeamPlayers($id: ID!) {
     team(id: $id) {
-      squad {
-        _id
-        firstName
-        lastName
+      coach {
+        id
         name
-        position
-        dateOfBirth
         nationality
-        shirtNumber
-        marketValue
+      }
+      squad(excludeStartingEleven: false) {
+        otherPlayers {
+          _id
+          name
+          age
+          nationality
+          position {
+            area
+            specialty
+          }
+        }
       }
     }
   }
 `;
 
+const initialValues = {
+  goalkeeper: [],
+  defence: [],
+  midfield: [],
+  offence: []
+}
+
 const TeamPlayers = () => {
   const { id } = useParams();
   const { loading, error, data } = useQuery<{ team: Team }>(QUERY, { variables: { id } });
 
+  const squad = useMemo(() => data?.team ? [
+    { title: "coach", players: [data.team.coach] }, 
+    ...Object
+      .entries(
+        data
+          .team
+          .squad
+          .otherPlayers
+          .reduce((playersObj, player) => ({ ...playersObj, [player.position.area]: [player, ...playersObj[player.position.area]] }), initialValues)
+      )
+      .map(([key, value]) => ({ title: key, players: value }))
+  ] : [], [data]);
+
   return (
-    <div className="p-4">
+    <div className="flex flex-col gap-4">
       {
-        loading ?
-          <PlayerLoading size={11} /> :
-          error ?
-            <ErrorMessage /> :
-            !data ?
-              <NothingWasFound /> :
-              <ul className="flex flex-col border border-secondary-900/50 rounded-md overflow-clip">
-                {
-                  data.team.squad.map((player, index) => <PlayerCard key={index} {...player} />)
-                }
-              </ul>
+        squad.map(({ title, players }) => (
+          <div key={title} className="rounded-xl p-4 flex flex-col gap-4 bg-[#191919] border border-transparent">
+            <h3 className='capitalize text-sm font-semibold text-white-100'>{/goalkeeper/.test(title) ? `${title}s` : title}</h3>
+            <ul className="grid grid-cols-4 gap-2">
+              {
+                players.map((player) => (
+                  <li key={player._id ? player._id : player.id ? player.id : player.name} className="flex items-center p-6 gap-4 bg-white-100/5 rounded-md">
+                    <span className="w-10 aspect-square max-h-10 rounded-full bg-white-100/10 flex items-end justify-center overflow-hidden">
+                      <IoPerson size={32} className='-mb-[2px] text-white-600' />
+                    </span>
+                    <div className="flex flex-col">
+                      <span className="text-xs font-semibold text-white-100">{player.name}</span>
+                      <span className="text-2xs text-white-700 capitalize">{player?.position?.specialty}</span>
+                    </div>
+                  </li>
+                ))
+              }
+            </ul>
+          </div>
+        ))
       }
     </div>
   );
