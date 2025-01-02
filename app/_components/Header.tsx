817e099d88fe20960ec506c516c3c2c3395e692a
Eunoia <ezemaemmanuel6@gmail.com>
2025-01-02 13:38:56 +0100

    Changed layout of header component

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -28,44 +28,46 @@ const Header = () => {
   }, []);
 
   return (
-    <div className="w-full py-2 gap-2">
-      <div className="w-full max-w-[var(--max-width)] m-auto flex justify-between items-center">
-        <div className="flex items-center gap-12">
-          <div className="h-[50px] z-[2] font-bold text-lg border-secondary-900/50 text-white-100 flex items-center justify-start gap-3">
-            <PiPlug size={24} /> 
-            <span>ScorePlug</span>
+    <div className="sticky top-0 z-10 w-full">
+      <div className="relative bg-[#191919] mb-[1px] z-10 w-full h-14 flex items-center justify-center">
+        <div className="w-full max-w-[var(--max-width)] m-auto flex justify-between items-center">
+          <div className="flex items-center gap-12">
+            <span className="font-inter font-bold text-xl text-white-100">ScorePlug.</span>
+
+            <div className="flex items-center h-8 rounded-full bg-white-100/10">
+              <label htmlFor="search" className="text-white-500 flex items-center justify-center w-8 aspect-square">
+                <MdSearch size={16} />
+              </label>
+              <input 
+                className="text-xs flex-1 bg-transparent border-none outline-none focus:outline-none text-white-500 placeholder:text-white-600 pr-2" 
+                type="text" 
+                id="search" 
+                placeholder="Barcelona v Chelsea" 
+              />
+            </div>
           </div>
-          <div className="flex items-center h-8 rounded-full bg-white-100/10">
-            <label htmlFor="search" className="text-white-500 flex items-center justify-center w-8 aspect-square">
-              <MdSearch size={16} />
-            </label>
-            <input 
-              className="text-xs flex-1 bg-transparent border-none outline-none focus:outline-none text-white-500 placeholder:text-white-600 pr-2" 
-              type="text" 
-              id="search" 
-              placeholder="Barcelona v Chelsea" 
-            />
-          </div>
+          <ul className="flex-1 flex items-stretch justify-end gap-4">
+            <li className="w-8 aspect-square rounded-full bg-white-100/10 text-white-600 flex items-center justify-center">
+              <MdOutlinePerson size={16} />
+            </li>
+            <li className="h-8 rounded-full bg-white-100/10 text-white-600 flex items-center justify-center gap-2 px-2">
+              <MdOutlineSettings size={16} />
+              <FaAngleDown size={12} />
+            </li>
+          </ul>
         </div>
-        <ul className="flex-1 flex items-stretch justify-end gap-4">
-          <li className="w-8 aspect-square rounded-full bg-white-100/10 text-white-600 flex items-center justify-center">
-            <MdOutlinePerson size={16} />
-          </li>
-          <li className="h-8 rounded-full bg-white-100/10 text-white-600 flex items-center justify-center gap-2 px-2">
-            <MdOutlineSettings size={16} />
-            <FaAngleDown size={12} />
-          </li>
-        </ul>
       </div>
-      <nav className="sticky top-0 z-[1] w-full mt-2 m-auto max-w-[var(--max-width)]">
-        <ul className={`flex items-center transition-transform w-fit ${scrolled && "lg:translate-x-[160px]"} gap-1`}>
-          {
-            navLinks.map((navLink, index) => (
-              <li className=""><NavLink key={index} {...navLink} /></li>
-            ))
-          }
-        </ul>
-      </nav>
+      <div className={`relative ${scrolled && 'lg:-translate-y-full'} transition-transform duration-300 bg-[#191919] flex items-center justify-center`}>
+        <nav className="py-2 w-full m-auto max-w-[var(--max-width)]">
+          <ul className={`flex items-center w-fit gap-1`}>
+            {
+              navLinks.map((navLink, index) => (
+                <li key={index} className=""><NavLink {...navLink} /></li>
+              ))
+            }
+          </ul>
+        </nav>
+      </div>
     </div>
   )
 }
