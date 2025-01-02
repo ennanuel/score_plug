03195801e58a350f863f791814d0a453578cb982
Eunoia <ezemaemmanuel6@gmail.com>
2025-01-02 13:36:48 +0100

    Modified component layout and refactored code

--- app/_components/MatchesContainer.tsx
+++ app/_components/MatchesContainer.tsx
@@ -7,7 +7,18 @@ import { SocketContext } from '../SocketContext';
 import { MatchLoading } from './loading';
 import ErrorMessage from './ErrorMessage';
 
-function MatchesContainer({ matches, loading, error }: { matches: Match[] | undefined, loading?: boolean; error?: boolean; }) {
+type Props = { 
+    matches: Match[] | undefined; 
+    loading?: boolean; 
+    error?: boolean; 
+    roundedMatches?: boolean; 
+    showDateAndCompetition?: boolean; 
+    showHalfTimeScore?: boolean;
+    small?: boolean;
+    teamId?: string;
+};
+
+function MatchesContainer({ matches, loading, error, roundedMatches, showDateAndCompetition, showHalfTimeScore, small, teamId }: Props) {
     const { socketData } = useContext(SocketContext);
     const matchesToView = useMemo(() => matches?.map(match => {
         const socketMatch = socketData.matches[match._id];
@@ -25,9 +36,20 @@ function MatchesContainer({ matches, loading, error }: { matches: Match[] | unde
     else if (error) return <ErrorMessage />;
 
     return (
-        <ul className='flex flex-col rounded-md border border-secondary-900/50 overflow-hidden'>
+        <ul className='flex flex-col'>
             {
-                matchesToView.map((match) => <MatchCard key={match._id} {...match} />)
+                matchesToView.map((match) => 
+                (
+                    <MatchCard 
+                        {...match}
+                        key={match._id}  
+                        rounded={roundedMatches} 
+                        showHalfTimeScore={showHalfTimeScore} 
+                        showDateAndCompetition={showDateAndCompetition} 
+                        small={small} 
+                        teamId={teamId} 
+                    />
+                ))
             }
         </ul>
     )
