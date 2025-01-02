6842d9f9e461e96d369b5d3a0b8948242a2c6e2f
Eunoia <ezemaemmanuel6@gmail.com>
2025-01-02 13:10:33 +0100

    Completely changed the design and layout of page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import { useQuery, gql } from '@apollo/client';
-import { ErrorMessage, NothingWasFound, CompetitionWithMatches } from './_components';
+import { ErrorMessage, NothingWasFound, CompetitionWithMatches, LoadingMessage } from './_components';
 import { CompetitionWithMatchesLoading } from './_components/loading';
 
 import { Competition } from '@/types/global.type';
@@ -11,21 +11,25 @@ import { FaAngleRight } from 'react-icons/fa6';
 import { BiCaretDown } from 'react-icons/bi';
 import { FaAngleLeft } from 'react-icons/fa';
 import { IoFilter } from 'react-icons/io5';
+import { COMPETITIONS } from './_assets/constants/competition';
+import { getDay } from './_utils/dateTime';
+import DateSelector from './_components/DateSelector';
 
 const QUERY = gql`
-  query GetActiveCompetitions($isLive: Boolean!) {
-    activeCompetitions(isLive: $isLive) {
+  query GetActiveCompetitions($isLive: Boolean!, $fromDate: String, $toDate: String) {
+    activeCompetitions(isLive: $isLive, from: $fromDate, to: $toDate) {
       _id
       name
-      emblem
       area {
         name
+        flag
       }
-      matches {
+      matches(from: $fromDate, to: $toDate) {
         _id
         status
         utcDate
         minute
+        predictionAvailable
         homeTeam {
           name
           crest
@@ -47,54 +51,63 @@ const QUERY = gql`
 
 function Home() {
   const [status, setStatus] = useState<"IN_PLAY" | "">("");
+  const [date, setDate] = useState({ 
+    fromDate: (new Date()).toDateString(), 
+    toDate: (new Date()).toDateString()
+  });
+  
+  const dateDisplayText = useMemo(() => (
+    /(today|tomorrow|yesterday)/i.test(getDay(date.fromDate, true)) ? 
+      `${getDay(date.fromDate, true)}'s matches` : 
+      `${getDay(date.fromDate, true)} matches`
+  ), [date]);
+
   const showLiveMatches = () => setStatus("IN_PLAY");
   const showAllMatches = () => setStatus("");
 
-  const { loading, error, data } = useQuery<{ activeCompetitions: Competition[] }>(QUERY, { variables: { isLive: status === 'IN_PLAY' } });
+  const toggleMatchStatus = status === 'IN_PLAY' ? showAllMatches : showLiveMatches;
+
+  const { loading, error, data } = useQuery<{ activeCompetitions: Competition[] }>(QUERY, { 
+    variables: { 
+      isLive: status === 'IN_PLAY',
+      ...date
+    } 
+  });
 
   if (error) return <ErrorMessage />;
 
   return (
     <main className="flex flex-col gap-4">
-      <div className="flex flex-col rounded-lg overflow-hidden border border-white-100/10 bg-white-100/5">
-        <div className="flex items-center justify-between gap-4 p-3">
-            <button className="flex items-center justify-center bg-white-100/10 rounded-full w-6 aspect-square text-white-500">
-              <FaAngleLeft size={12} />
-            </button>
-            <button className="flex items-center justify-center w-fit gap-2 px-2 text-white-400 hover:text-white-600">
-              <span className="font-bold text-sm">Today's matches</span>
-              <BiCaretDown size={12} />
-            </button>
-          <button className="flex items-center justify-center bg-white-100/10 rounded-full w-6 aspect-square text-white-500">
-            <FaAngleRight size={12} />
-          </button>
+      <div className="flex flex-col rounded-xl overflow-hidden border border-transparent bg-white-100/10">
+        <div className="p-3">
+          <DateSelector setDate={setDate} displayText={dateDisplayText} useCurrentDate />
         </div>
-        <div className="border-t border-white-100/5 flex justify-between gap-2 p-3">
+        <div className="border-t border-white-100/10 flex justify-between gap-2 p-3">
           <div className="flex items-center gap-2">
-            <button className="flex items-center justify-center gap-2 text-white-500 h-8 rounded-full bg-white/10 border border-white-100/10 px-4">
-              <span className="text-xs font-semibold">Live</span>
+            <button onClick={toggleMatchStatus} className={`${status === "IN_PLAY" ? 'border-green-400 bg-green-400 text-black-900' : 'text-white-600 border-transparent bg-white-100/10 hover:text-white-500 hover:bg-white-100/20'} flex items-center justify-center gap-2 h-8 rounded-full border px-4`}>
+              <span className="text-2xs font-semibold">Live</span>
             </button>
-            <button className="flex items-center justify-center gap-2 text-white-500 h-8 rounded-full bg-white/10 border border-white-100/10 px-4">
-              <span className="text-xs font-semibold">By Time</span>
+            <button className="flex items-center justify-center gap-2 text-white-600 hover:bg-white-100/20 hover:text-white-500 h-8 rounded-full bg-white-100/10 border border-transparent px-4">
+              <span className="text-2xs font-semibold">By time</span>
             </button>
           </div>
-          <span className="flex-1 flex items-center justify-center gap-3 text-white-500 h-8 rounded-full bg-white/10 border border-white-100/10 px-4">
-            <IoFilter size={14} />
-            <input type="text" placeholder="Filter" className='flex-1 text-xs font-semibold h-full bg-transparent focus:outline-none outline-none text-white-500 placeholder:text-white-600' />
+          <span className="flex-1 flex items-center justify-center gap-3 text-white-500 h-8 rounded-full bg-transparent border border-white-100/10 px-4">
+            <IoFilter size={12} />
+            <input type="text" placeholder="Filter" className='flex-1 text-2xs font-normal h-full bg-transparent focus:outline-none outline-none text-white-500 placeholder:text-white-600' />
           </span>
         </div>
       </div>
-      {
-        loading ?
-          <CompetitionWithMatchesLoading size={4} /> :
-          data?.activeCompetitions?.length === 0 ?
-            <NothingWasFound /> :
-            <ul className="flex flex-col">
-              {
-                data?.activeCompetitions.map((competition, index) => <CompetitionWithMatches key={index} {...competition} />)
-              }
-            </ul>
-      }
+      <section className="flex flex-col gap-4">
+        {
+          loading ?
+            <CompetitionWithMatchesLoading size={2} /> :
+            data
+            ?.activeCompetitions
+            ?.map((competition, index) => (
+              <CompetitionWithMatches {...competition} key={index} />
+            ))
+        }
+      </section>
     </main>
   )
 }
