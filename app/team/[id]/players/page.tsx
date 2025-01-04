0f520ba3110a17cac5df7e836e61f516d866e88a
Eunoia <ezemaemmanuel6@gmail.com>
2025-01-04 16:03:58 +0100

    Implemented loading functionality and made page responsive

--- app/team/[id]/players/page.tsx
+++ app/team/[id]/players/page.tsx
@@ -6,6 +6,7 @@ import React, { useMemo } from 'react';
 
 import { Team } from "@/types/global.type";
 import { IoPerson } from 'react-icons/io5';
+import { DetailsLoading } from '@/app/_components/loading';
 
 const QUERY = gql`
   query GetTeamPlayers($id: ID!) {
@@ -55,16 +56,18 @@ const TeamPlayers = () => {
       .map(([key, value]) => ({ title: key, players: value }))
   ] : [], [data]);
 
+  if(loading) return <DetailsLoading />
+
   return (
     <div className="flex flex-col gap-4">
       {
         squad.map(({ title, players }) => (
-          <div key={title} className="rounded-xl p-4 flex flex-col gap-4 bg-[#191919] border border-transparent">
+          <div key={title} className="rounded-xl py-4 px-3 sm:px-4 flex flex-col gap-4 bg-[#191919] border border-transparent">
             <h3 className='capitalize text-sm font-semibold text-white-100'>{/goalkeeper/.test(title) ? `${title}s` : title}</h3>
-            <ul className="grid grid-cols-4 gap-2">
+            <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
               {
                 players.map((player) => (
-                  <li key={player._id ? player._id : player.id ? player.id : player.name} className="flex items-center p-6 gap-4 bg-white-100/5 rounded-md">
+                  <li key={player._id ? player._id : player.id ? player.id : player.name} className="flex items-center p-4 sm:p-6 gap-4 bg-white-100/5 rounded-md">
                     <span className="w-10 aspect-square max-h-10 rounded-full bg-white-100/10 flex items-end justify-center overflow-hidden">
                       <IoPerson size={32} className='-mb-[2px] text-white-600' />
                     </span>
