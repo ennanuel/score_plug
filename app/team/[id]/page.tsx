5a17990e87fb1e14795ff9bd9bfa0b27f5681225
Eunoia <ezemaemmanuel6@gmail.com>
2025-01-04 16:02:14 +0100

    Made page responsive

--- app/team/[id]/page.tsx
+++ app/team/[id]/page.tsx
@@ -205,70 +205,74 @@ const TeamInfo = () => {
     else if (!data) return <NothingWasFound />;
 
     return (
-        <div className="grid grid-cols-[1fr,_360px] gap-4">
+        <div className="grid grid-cols-1 lg:grid-cols-[1fr,_360px] gap-4">
             <div className="flex flex-col gap-4">
-                <div className="grid grid-cols-2 gap-4">
-                    <div className="flex flex-col gap-6 rounded-xl bg-white-100/10 border border-transparent p-6">
-                        <h3 className="text-sm text-white-100 font-semibold">Team Form</h3>
-                        <ul className="flex justify-between gap-4">
-                            {
-                                data.team.matches.map(({ _id, homeTeam, awayTeam, score }) => (
-                                    <li key={_id} className="flex flex-col items-center justify-center gap-4">
-                                        <span className={`${(String(homeTeam._id) === String(id) && score.fullTime.home > score.fullTime.away) || (String(awayTeam._id) === String(id) && score.fullTime.away > score.fullTime.home) ? 'bg-green-600' : score.fullTime.home === score.fullTime.away ? 'bg-gray-500' : 'bg-red-500'} px-2 h-6 relative flex items-center justify-center text-2xs font-semibold text-white-100 rounded-md after:absolute ${(String(homeTeam._id) === String(id) && score.fullTime.home > score.fullTime.away) || (String(awayTeam._id) === String(id) && score.fullTime.away > score.fullTime.home) ? 'after:bg-green-600' : ''} after:-bottom-1 after:left-1/2 after:-translate-x-1/2 after:w-[calc(100%_-_16px)] after:h-[2px] after:rounded-full`}>{score.fullTime.home} - {score.fullTime.away}</span>
-                                        <Image src={String(homeTeam._id) === String(id) ? awayTeam.crest : homeTeam.crest} alt={`${data.team.name} crest`} width={24} height={24} className="w-6 aspect-square object-contain" />
-                                    </li>
-                                ))
-                            }
-                        </ul>
-                    </div>
-                    <Link href={`/match/${(nextFixture ? nextFixture : data.team.matches[0])?._id}`} className="group flex flex-col gap-8 rounded-xl bg-white-100/10  border border-transparent p-4 pb-6">
-                        <div className="flex items-center justify-between gap-4">
-                            <h3 className="text-sm text-white-100 font-semibold">{nextFixture ? 'Next match' : 'Last match'}</h3>
-                            <div className="flex items-center justify-center gap-2">
-                                <span className="text-2xs text-white-700">{(nextFixture ? nextFixture : data.team.matches[0])?.competition?.name}</span>
-                                <span className="flex items-center justify-center w-6 aspect-square max-h-6 rounded-full border border-white-100/20">
-                                    <Image src={(nextFixture ? nextFixture : data.team.matches[0])?.competition?.emblem} alt={`${(nextFixture ? nextFixture : data.team.matches[0])?.competition?.name} crest`} height={24} width={24} className="w-4 max-h-4 aspect-square obtain-cover" />
-                                </span>
-                            </div>
-                        </div>
-                        <div className="group-hover:opacity-60 grid grid-cols-[1fr,_auto,_1fr] gap-6">
-                            <div className="flex flex-col items-center justify-end gap-4">
-                                <Image src={(nextFixture ? nextFixture : data.team.matches[0])?.homeTeam?.crest} alt={`${(nextFixture ? nextFixture : data.team.matches[0])?.homeTeam?.name} crest`} height={24} width={24} className="w-6 max-h-6 aspect-square object-contain" />
-                                <span className="text-2xs text-white-100">{(nextFixture ? nextFixture : data.team.matches[0])?.homeTeam?.shortName}</span>
-                            </div>
-                            <div className="flex flex-col gap-4">
-                                <span className="text-3xl font-semibold text-white-100">
+                {
+                    data.team.matches.length > 0 || data.teamNextFixtures.length > 0 ?
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                            <div className="flex flex-col gap-6 rounded-xl bg-white-100/10 border border-transparent p-3 sm:p-6">
+                                <h3 className="text-sm text-white-100 font-semibold">Team Form</h3>
+                                <ul className="flex justify-between gap-4">
                                     {
-                                        (nextFixture ? nextFixture : data.team.matches[0])?.status === 'TIMED' ?
-                                        <span>{getTimeFormat((nextFixture ? nextFixture : data.team.matches[0])?.utcDate)}</span> :
-                                        <span>{`${(nextFixture ? nextFixture : data.team.matches[0]).score.fullTime.home} - ${(nextFixture ? nextFixture : data.team.matches[0]).score.fullTime.away}`}</span>
+                                        data.team.matches.map(({ _id, homeTeam, awayTeam, score }) => (
+                                            <li key={_id} className="flex flex-col items-center justify-center gap-4">
+                                                <span className={`${(String(homeTeam._id) === String(id) && score.fullTime.home > score.fullTime.away) || (String(awayTeam._id) === String(id) && score.fullTime.away > score.fullTime.home) ? 'bg-green-600' : score.fullTime.home === score.fullTime.away ? 'bg-gray-500' : 'bg-red-500'} px-2 h-6 relative flex items-center justify-center text-2xs font-semibold text-white-100 rounded-md after:absolute ${(String(homeTeam._id) === String(id) && score.fullTime.home > score.fullTime.away) || (String(awayTeam._id) === String(id) && score.fullTime.away > score.fullTime.home) ? 'after:bg-green-600' : ''} whitespace-nowrap after:-bottom-1 after:left-1/2 after:-translate-x-1/2 after:w-[calc(100%_-_16px)] after:h-[2px] after:rounded-full`}>{score.fullTime.home} - {score.fullTime.away}</span>
+                                                <Image src={String(homeTeam._id) === String(id) ? awayTeam.crest : homeTeam.crest} alt={`${data.team.name} crest`} width={24} height={24} className="w-6 aspect-square object-contain" />
+                                            </li>
+                                        ))
                                     }
-                                </span>
-                                <span className="text-2xs flex items-center justify-center">
-                                    {
-                                        (nextFixture ? nextFixture : data.team.matches[0])?.status === "TIMED" ?
-                                            <span className="text-white-700">{getDay((nextFixture ? nextFixture : data.team.matches[0])?.utcDate)}</span> :
-                                            /in_play|finished|paused/i.test((nextFixture ? nextFixture : data.team.matches[0])?.status) ?
-                                            <span className={`${(nextFixture ? nextFixture : data.team.matches[0])?.status !== 'FINISHED' ? 'text-yellow-400' : 'text-white-700'}`}>
-                                                {
-                                                    (nextFixture ? nextFixture : data.team.matches[0])?.minute === 'FT' ? 
-                                                        'Full time' :
-                                                        (nextFixture ? nextFixture : data.team.matches[0])?.minute === 'HT' ?
-                                                            'Half time' :
-                                                    `${(nextFixture ? nextFixture : data.team.matches[0])?.minute}'`
-                                                }
-                                            </span> :
-                                            <span className="text-white-700">{(nextFixture ? nextFixture : data.team.matches[0])?.status?.toLowerCase()}</span>
-                                    }
-                                </span>
+                                </ul>
                             </div>
-                            <div className="flex flex-col items-center justify-start gap-4">
-                                <Image src={(nextFixture ? nextFixture : data.team.matches[0])?.awayTeam?.crest} alt={`${(nextFixture ? nextFixture : data.team.matches[0])?.awayTeam?.name} crest`} height={24} width={24} className="w-6 max-h-6 aspect-square object-contain" />
-                                <span className="text-2xs text-white-100">{(nextFixture ? nextFixture : data.team.matches[0])?.awayTeam?.shortName}</span>
-                            </div>
-                        </div>
-                    </Link>
-                </div>
+                            <Link href={`/match/${(nextFixture ? nextFixture : data.team.matches[0])?._id}`} className="group flex flex-col gap-8 rounded-xl bg-white-100/10  border border-transparent px-3 pb-4 pt-3 sm:pt-4 sm:px-6 sm:pb-6">
+                                <div className="flex items-center justify-between gap-4">
+                                    <h3 className="text-sm text-white-100 font-semibold">{nextFixture ? 'Next match' : 'Last match'}</h3>
+                                    <div className="flex items-center justify-center gap-2">
+                                        <span className="text-2xs text-white-700">{(nextFixture ? nextFixture : data.team.matches[0])?.competition?.name}</span>
+                                        <span className="flex items-center justify-center w-6 aspect-square max-h-6 rounded-full border border-white-100/20">
+                                            <Image src={(nextFixture ? nextFixture : data.team.matches[0])?.competition?.emblem} alt={`${(nextFixture ? nextFixture : data.team.matches[0])?.competition?.name} crest`} height={24} width={24} className="w-4 max-h-4 aspect-square obtain-cover" />
+                                        </span>
+                                    </div>
+                                </div>
+                                <div className="group-hover:opacity-60 grid grid-cols-[1fr,_auto,_1fr] gap-6">
+                                    <div className="flex flex-col items-center justify-end gap-4">
+                                        <Image src={(nextFixture ? nextFixture : data.team.matches[0])?.homeTeam?.crest} alt={`${(nextFixture ? nextFixture : data.team.matches[0])?.homeTeam?.name} crest`} height={24} width={24} className="w-6 max-h-6 aspect-square object-contain" />
+                                        <span className="text-2xs text-white-100">{(nextFixture ? nextFixture : data.team.matches[0])?.homeTeam?.shortName}</span>
+                                    </div>
+                                    <div className="flex flex-col items-center justify-center gap-4">
+                                        <span className="text-3xl font-semibold text-white-100">
+                                            {
+                                                (nextFixture ? nextFixture : data.team.matches[0])?.status === 'TIMED' ?
+                                                <span>{getTimeFormat((nextFixture ? nextFixture : data.team.matches[0])?.utcDate)}</span> :
+                                                <span>{`${(nextFixture ? nextFixture : data.team.matches[0])?.score?.fullTime?.home} - ${(nextFixture ? nextFixture : data?.team?.matches[0])?.score?.fullTime?.away}`}</span>
+                                            }
+                                        </span>
+                                        <span className="text-2xs flex items-center justify-center">
+                                            {
+                                                (nextFixture ? nextFixture : data.team.matches[0])?.status === "TIMED" ?
+                                                    <span className="text-white-700">{getDay((nextFixture ? nextFixture : data.team.matches[0])?.utcDate)}</span> :
+                                                    /in_play|finished|paused/i.test((nextFixture ? nextFixture : data.team.matches[0])?.status) ?
+                                                    <span className={`${(nextFixture ? nextFixture : data.team.matches[0])?.status !== 'FINISHED' ? 'text-yellow-400' : 'text-white-700'}`}>
+                                                        {
+                                                            (nextFixture ? nextFixture : data.team.matches[0])?.minute === 'FT' ? 
+                                                                'Full time' :
+                                                                (nextFixture ? nextFixture : data.team.matches[0])?.minute === 'HT' ?
+                                                                    'Half time' :
+                                                            `${(nextFixture ? nextFixture : data.team.matches[0])?.minute}'`
+                                                        }
+                                                    </span> :
+                                                    <span className="text-white-700">{(nextFixture ? nextFixture : data.team.matches[0])?.status?.toLowerCase()}</span>
+                                            }
+                                        </span>
+                                    </div>
+                                    <div className="flex flex-col items-center justify-start gap-4">
+                                        <Image src={(nextFixture ? nextFixture : data.team.matches[0])?.awayTeam?.crest} alt={`${(nextFixture ? nextFixture : data.team.matches[0])?.awayTeam?.name} crest`} height={24} width={24} className="w-6 max-h-6 aspect-square object-contain" />
+                                        <span className="text-2xs text-white-100">{(nextFixture ? nextFixture : data.team.matches[0])?.awayTeam?.shortName}</span>
+                                    </div>
+                                </div>
+                            </Link>
+                        </div> :
+                        null
+                }
                 <div>
                     <Standings competition={data.team.league} teams={[data.team._id]} showCompetition />
                 </div>
@@ -282,22 +286,26 @@ const TeamInfo = () => {
                         </div>
                     </TeamOfTheWeek>
                 </div>
-                <div className="flex flex-col rounded-xl border border-transparent bg-[#191919] pb-2">
-                    <div className="flex justify-between gap-4 items-center p-4">
-                        <button className="w-6 rounded-full aspect-square flex items-center justify-center bg-white-100/10 text-white-100 hover:bg-white-100 hover:text-black-900">
-                            <FaAngleLeft size={16} />
-                        </button>
-                        <span className="text-xs font-semibold text-white-100">Fixtures</span>
-                        <button className="w-6 rounded-full aspect-square flex items-center justify-center bg-white-100/10 text-white-100 hover:bg-white-100 hover:text-black-900">
-                            <FaAngleLeft size={16} />
-                        </button>
-                    </div>
-                    <MatchesContainer 
-                        matches={[...data.teamNextFixtures, ...data.team.matches.slice(0, 5 - data.teamNextFixtures.length)]} 
-                        showDateAndCompetition 
-                        small
-                    />
-                </div>
+                {
+                    data.team.matches.length > 0 || data.teamNextFixtures.length > 0 ?
+                        <div className="flex flex-col rounded-xl border border-transparent bg-[#191919] pb-2">
+                            <div className="flex justify-between gap-4 items-center p-4">
+                                <button className="w-6 rounded-full aspect-square flex items-center justify-center bg-white-100/10 text-white-100 hover:bg-white-100 hover:text-black-900">
+                                    <FaAngleLeft size={16} />
+                                </button>
+                                <span className="text-xs font-semibold text-white-100">Fixtures</span>
+                                <button className="w-6 rounded-full aspect-square flex items-center justify-center bg-white-100/10 text-white-100 hover:bg-white-100 hover:text-black-900">
+                                    <FaAngleLeft size={16} />
+                                </button>
+                            </div>
+                            <MatchesContainer 
+                                matches={[...data.teamNextFixtures, ...data.team.matches.slice(0, 5 - data.teamNextFixtures.length)]} 
+                                showDateAndCompetition 
+                                small
+                            />
+                        </div> :
+                        null
+                }
                 <div className="flex flex-col rounded-xl border border-transparent bg-[#191919] p-4 gap-6">
                     <h3 className="text-xs font-semibold text-white-100">Stadium</h3>
                     <div className="flex items-center justify-start gap-3">
