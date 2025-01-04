30a8de1797a4acb398f43d2012b3042bfd858d53
Eunoia <ezemaemmanuel6@gmail.com>
2025-01-04 16:26:31 +0100

    made component responsive

--- app/_components/AltHeader.tsx
+++ app/_components/AltHeader.tsx
@@ -3,7 +3,7 @@ import AltNavLink from "./AltNavLink";
 const AltHeader = ({ links }: { links: { title: string; href: string; }[] }) => {
 
     return (
-        <ul className="flex px-6 gap-6">
+        <ul className="flex px-4 lg:px-6 gap-6">
             {links.map((link, index) => <AltNavLink key={index} {...link} />)}
         </ul>
     )
--- app/_components/AltNavLink.tsx
+++ app/_components/AltNavLink.tsx
@@ -7,7 +7,7 @@ const NavLink = ({ href, title }: { href: string; title: string }) => {
 
     return (
         <Link href={href} className={`relative group flex flex-col gap-4 items-center justify-center after:w-full after:h-[2px] after:rounded-t-full ${active ? 'after:bg-white-100' : 'hover:text-white-100'}`}>
-            <span className={`${active ? 'text-white-300' : 'text-white-700 group-hover:text-white-600'} font-semibold text-xs`}>{title}</span>
+            <span className={`${active ? 'text-white-300' : 'text-white-700 group-hover:text-white-600'} font-semibold text-2xs sm:text-xs`}>{title}</span>
         </Link>
     )
 };
