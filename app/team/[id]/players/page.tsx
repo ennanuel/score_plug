0d55e846875352f6417a302701f10e3dff005736
Eunoia <ezemaemmanuel6@gmail.com>
2025-01-07 06:17:41 +0100

    Implemented logic for when nothing was found

--- app/team/[id]/players/page.tsx
+++ app/team/[id]/players/page.tsx
@@ -7,6 +7,7 @@ import React, { useMemo } from 'react';
 import { Team } from "@/types/global.type";
 import { IoPerson } from 'react-icons/io5';
 import { DetailsLoading } from '@/app/_components/loading';
+import { ErrorMessage, NothingWasFound } from '@/app/_components';
 
 const QUERY = gql`
   query GetTeamPlayers($id: ID!) {
@@ -56,31 +57,34 @@ const TeamPlayers = () => {
       .map(([key, value]) => ({ title: key, players: value }))
   ] : [], [data]);
 
-  if(loading) return <DetailsLoading />
+  if(loading) return <DetailsLoading />;
+  else if(error) return <ErrorMessage />;
 
   return (
     <div className="flex flex-col gap-4">
       {
-        squad.map(({ title, players }) => (
-          <div key={title} className="rounded-xl py-4 px-3 sm:px-4 flex flex-col gap-4 bg-[#191919] border border-transparent">
-            <h3 className='capitalize text-sm font-semibold text-white-100'>{/goalkeeper/.test(title) ? `${title}s` : title}</h3>
-            <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
-              {
-                players.map((player) => (
-                  <li key={player._id ? player._id : player.id ? player.id : player.name} className="flex items-center p-4 sm:p-6 gap-4 bg-white-100/5 rounded-md">
-                    <span className="w-10 aspect-square max-h-10 rounded-full bg-white-100/10 flex items-end justify-center overflow-hidden">
-                      <IoPerson size={32} className='-mb-[2px] text-white-600' />
-                    </span>
-                    <div className="flex flex-col">
-                      <span className="text-xs font-semibold text-white-100">{player.name}</span>
-                      <span className="text-2xs text-white-700 capitalize">{player?.position?.specialty}</span>
-                    </div>
-                  </li>
-                ))
-              }
-            </ul>
-          </div>
-        ))
+        !loading && !squad.length ?
+          <NothingWasFound noBackground /> :
+          squad.map(({ title, players }) => (
+            <div key={title} className="rounded-xl py-4 px-3 sm:px-4 flex flex-col gap-4 bg-[#191919] border border-transparent">
+              <h3 className='capitalize text-sm font-semibold text-white-100'>{/goalkeeper/.test(title) ? `${title}s` : title}</h3>
+              <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
+                {
+                  players.map((player) => (
+                    <li key={player._id ? player._id : player.id ? player.id : player.name} className="flex items-center p-4 sm:p-6 gap-4 bg-white-100/5 rounded-md">
+                      <span className="w-10 aspect-square max-h-10 rounded-full bg-white-100/10 flex items-end justify-center overflow-hidden">
+                        <IoPerson size={32} className='-mb-[2px] text-white-600' />
+                      </span>
+                      <div className="flex flex-col">
+                        <span className="text-xs font-semibold text-white-100">{player.name}</span>
+                        <span className="text-2xs text-white-700 capitalize">{player?.position?.specialty}</span>
+                      </div>
+                    </li>
+                  ))
+                }
+              </ul>
+            </div>
+          ))
       }
     </div>
   );
