352132207e8196a5f4edf1a75ebc5763917e60bf
Eunoia <ezemaemmanuel6@gmail.com>
2025-01-07 06:30:04 +0100

    Added functionality to buttons and implemented logic for error, and when nothing is found

--- app/competitions/page.tsx
+++ app/competitions/page.tsx
@@ -3,7 +3,7 @@
 import CompetitionCard from '../_components/CompetitionCard';
 import { Competition } from '@/types/global.type';
 import { useQuery, gql } from '@apollo/client';
-import { ErrorMessage } from '../_components';
+import { ErrorMessage, NothingWasFound } from '../_components';
 import { useContext, useMemo, useState } from 'react';
 import { SocketContext } from '../SocketContext';
 import { CompetitionLoading } from '../_components/loading';
@@ -18,6 +18,7 @@ const QUERY = gql`
             _id
             name
             emblem
+            ranking
             area {
                 name
                 flag
@@ -31,56 +32,58 @@ const QUERY = gql`
   }
 `;
 
+const OPTIONS = [
+    { title: "By name", value: "name" },
+    { title: "By ranking", value: "ranking" },
+    { title: "By activity", value: "activity" }
+]
+
 const Competitions = () => {
     const [limit, setLimit] = useState(10);
+    const [sortType, setSortType] = useState("name");
 
     const { loading, error, data } = useQuery<{ competitions: { competitions: Competition[]; totalPages: number; } }>(QUERY, { variables: { limit } });
 
     const { socketData } = useContext(SocketContext);
-    const competitions = useMemo(() => data?.competitions?.competitions?.map((competition) => ({
-        ...competition,
-        recentMatches: { ...competition.recentMatches, hasLiveMatch: socketData.competitions.includes(competition._id) }
-    })), [socketData, data])
+    const competitions = useMemo(() => data
+        ?.competitions
+        ?.competitions
+        ?.map((competition) => ({
+            ...competition,
+            recentMatches: { ...competition.recentMatches, hasLiveMatch: socketData.competitions.includes(competition._id) }
+        }))
+        ?.slice()
+        ?.sort((compA, compB) => (
+                sortType === 'name' ? 
+                    compA.name.toLowerCase().localeCompare(compB.name.toLowerCase()) : 
+                        sortType === 'ranking' ? 
+                        compA.ranking - compB.ranking : 
+                    compB.recentMatches.matches - compA.recentMatches.matches
+    )), [socketData, data, sortType]);
+
+    if(error) return <ErrorMessage />
 
     return (
         <div className="flex flex-col gap-6 items-center">
             <div className="w-full pb-3 flex flex-col gap-4 bg-white-100/10 border border-transparent rounded-xl">
-                <div className="flex items-center justify-between gap-4 p-3">
-                    <div className="hidden md:flex items-center gap-2">
-                        {
-                            ['By name', 'By ranking', 'By activity'].map((sortType, index) => (
-                                <button key={index} className={`flex items-center justify-center gap-1 px-3 h-8 rounded-full ${index === 0 ? 'bg-white-100 text-black-900' : 'text-white-500 hover:text-white-300 hover:bg-white-100/5 border border-white-100/10'}`}>
-                                    <span className="text-2xs font-semibold whitespace-nowrap">{sortType}</span>
-                                </button>
-                            ))
-                        }
-                    </div>
-                    <div className="flex md:hidden relative">
-                        <button className='flex items-center justify-center gap-1 h-8 px-3 rounded-full bg-white-100/10 text-white-600 focus:bg-white-100 focus:text-black-900'>
-                            <span className="text-2xs font-semibold whitespace-nowrap">By name</span>
-                        </button>
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <button className="flex items-center justify-center w-6 aspect-square rounded-full bg-white-100/10 text-white-500 hover:bg-white-100/60 hover:text-black-900">
-                            <FiArrowUp size={14} />
-                        </button>
-                        <button className="flex items-center justify-center w-6 aspect-square rounded-full bg-white-100 text-black-900">
-                            <FiArrowDown size={14} />
-                        </button>
-                    </div>
+                <div className="flex items-center gap-2 p-3">
+                    {
+                        OPTIONS.map(({ title, value }) => (
+                            <button key={value} onClick={() => setSortType(value)} className={`flex items-center justify-center gap-1 px-2 md:px-3 h-7 md:h-8 rounded-full ${sortType === value ? 'bg-white-100 text-black-900' : 'text-white-500 hover:text-white-300 hover:bg-white-100/5 border border-white-100/10'}`}>
+                                <span className="text-2xs font-semibold whitespace-nowrap">{title}</span>
+                            </button>
+                        ))
+                    }
                 </div>
                 <div className="px-3">
                     {
-                        loading ? (
-                            error ?
-                                <ErrorMessage /> :
-                                <CompetitionLoading size={8} />
-                        ) :
-                        <ul className="flex flex-col gap-3">
-                            {
-                                competitions?.map((competition, index) => <CompetitionCard {...competition} key={index} />)
-                            }
-                        </ul>
+                        loading ? 
+                            <CompetitionLoading size={8} /> :
+                            !loading && !competitions?.length ?
+                                <NothingWasFound text="Could not find competitions" /> :
+                            <ul className="flex flex-col gap-3">
+                                { competitions?.map((competition, index) => <CompetitionCard {...competition} key={index} />) }
+                            </ul>
                     }
                 </div>
             </div>
