aac1c9cdefda73f0d3f005a9508a1c3a98b956ac
Emmanuel Ezema <emmanuelzema6@gmail.com>
2025-05-24 22:35:14 +0100

    Removed 'loader' property from 'Image' component and refactored code

--- app/_components/CompetitionCard.tsx
+++ app/_components/CompetitionCard.tsx
@@ -3,13 +3,19 @@
 import { Competition } from '@/types/global.type';
 import Link from 'next/link';
 import Image from "next/image";
-import { loadImage } from '../_utils/competition';
 
 const CompetitionCard = ({ _id, name, emblem, area, recentMatches }: Competition) => {
     return (
         <li className={`bg-white-100/5 rounded-lg hover:bg-white-100/10 p-3 min-h-14 flex`}>
             <Link href={`/competition/${_id}`} className="flex items-center gap-4 justify-between w-full">
-                <Image src={emblem || String(process.env.NEXT_IMAGE_URI)} loader={loadImage} width={30} height={30} className="w-6 max-h-6 aspect-square object-contain" alt={name} />
+                <Image 
+                    unoptimized 
+                    width={30} 
+                    height={30} 
+                    src={emblem || String(process.env.NEXT_IMAGE_URI)} 
+                    className="w-6 max-h-6 aspect-square object-contain" 
+                    alt={name} 
+                />
                 <div className="flex-1 flex flex-col">
                     <div className="flex items-center justify-between gap-4">
                         <h3 className={`${recentMatches.hasLiveMatch ? 'text-yellow-400' : 'text-white-400'} font-semibold text-2xs`}>{name}</h3>
