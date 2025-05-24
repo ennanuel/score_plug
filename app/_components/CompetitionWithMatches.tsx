7aeaf0a00fcfc938bec3668a37d6af02baf92e61
Emmanuel Ezema <emmanuelzema6@gmail.com>
2025-05-24 22:33:44 +0100

    Removed 'loader' property from 'Image' component and removed any unused imports

--- app/_components/CompetitionWithMatches.tsx
+++ app/_components/CompetitionWithMatches.tsx
@@ -4,17 +4,16 @@ import Link from "next/link";
 import Image from "next/image";
 import { MatchesContainer } from '.';
 import { Competition } from '@/types/global.type';
-import { loadImage } from '../_utils/competition';
 
 const CompetitionWithMatches = ({ _id, name, area, matches }: Competition) => {
     return (
         <li className='flex flex-col border border-transparent bg-white-100/10 rounded-xl overflow-hidden'>
             <Link href={`/competition/${_id}`} className='flex items-center bg-white-100/5 hover:bg-white-100/10 gap-3 p-3 border-b border-transparent'>
                 <Image 
-                    src={area.flag || String(process.env.NEXT_IMAGE_URI)} 
-                    loader={loadImage} 
+                    unoptimized
                     width={16} 
                     height={16} 
+                    src={area.flag || String(process.env.NEXT_IMAGE_URI)} 
                     alt={name} 
                     className="aspect-square object-cover rounded-full" 
                 />
